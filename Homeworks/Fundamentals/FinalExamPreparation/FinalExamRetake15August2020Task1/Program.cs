3788138737e2608f81085627d46439d3adafc8f4
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-15 22:20:39 +0300

    Try to add a single repo

---
+++ Homeworks/Fundamentals/FinalExamPreparation/FinalExamRetake15August2020Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalExamRetake15August2020Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
