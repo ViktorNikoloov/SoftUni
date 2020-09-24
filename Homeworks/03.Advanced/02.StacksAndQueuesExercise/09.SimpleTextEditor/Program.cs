1c3694b89e4c755138aad6ef206e57907b6c5964
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-24 16:13:42 +0300

    Add new tasks

---
+++ Homeworks/03.Advanced/02.StacksAndQueuesExercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            Stack<string> oldTexts = new Stack<string>();
+            oldTexts.Push(text.ToString());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int command = int.Parse(input[0]);
+
+                if (command == 1) //appends
+                {
+                    string newText = input[1];
+                    text.Append(newText);
+                    oldTexts.Push(text.ToString());
+                }
+                else if (command == 2) //erases 
+                {
+                    int count = int.Parse(input[1]);
+                    text.Remove(text.Length - count, count);
+                    oldTexts.Push(text.ToString());
+                }
+                else if (command == 3) //returns
+                {
+                    int index = int.Parse(input[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (command == 4) // 1 2 3 4
+                {
+                    oldTexts.Pop();
+                    text = new StringBuilder();
+                    text.Append(oldTexts.Peek());
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/03.Advanced/02.StacksAndQueuesExercise/10.Crossroads++/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace _10.Crossroads__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            Queue<string> cars = new Queue<string>();
+            bool isSafety = false;
+            string currCar = string.Empty;
+
+            while (command != "END")
+            {
+                cars.Enqueue(command);
+
+                if (command == "green")
+                {
+                    Queue<char> crossingCar = new Queue<char>(cars.Dequeue());
+                    currCar = crossingCar.ToString();
+
+                    for (int i = 0; i < greenLight; i++)
+                    {
+                        if (crossingCar.Count == 0)
+                        {
+                            crossingCar = new Queue<char>(cars.Dequeue());
+                            currCar = crossingCar.ToString();
+
+                        }
+                        crossingCar.Dequeue();
+                    }
+
+                    for (int i = 0; i < freeWindow; i++)
+                    {
+                        if (crossingCar.Count == 0)
+                        {
+                            break;
+                        }
+
+                        crossingCar.Dequeue();
+                        
+                    }
+
+                    if (crossingCar.Count == 0)
+                    {
+                        isSafety = true;
+                    }
+
+                }
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
