33d115f420a79a2216638129698997d7d7e7936c
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-24 14:11:04 +0300

    Add tasks from Stacks and Queues exercise

---
+++ Homeworks/03.Advanced/02.StacksAndQueuesExercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (command[0] == 1)
+                {
+
+                    stack.Push(command[1]);
+
+                }
+                if (stack.Count > 0)
+                {
+                    if (command[0] == 2)
+                    {
+                        stack.Pop();
+                    }
+                    else if (command[0] == 3)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                    else if (command[0] == 4)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
---
+++ Homeworks/03.Advanced/02.StacksAndQueuesExercise/07.TruckTour/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> circle = new Queue<string>();
+
+            int numberOfPopmps = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPopmps; i++)
+            {
+                string input = Console.ReadLine();
+                input += $" {i}";
+                circle.Enqueue(input);
+            }
+
+            int petrol = 0;
+           
+            for (int i = 0; i < numberOfPopmps; i++)
+            {
+                string petrolInfo = circle.Dequeue();
+                int[] infoSplited = petrolInfo.Split().Select(int.Parse).ToArray();
+
+                petrol += infoSplited[0];
+                int distance = infoSplited[1];
+
+                if (!(petrol >= distance))
+                {
+                    circle.Enqueue(petrolInfo);
+                    petrol = 0;
+                    i = -1;
+                }
+                else
+                {
+                    circle.Enqueue(petrolInfo);
+                    petrol -= distance;
+                }
+
+            }
+            int[] indexe = circle.Dequeue().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(indexe[2]);
+        }
+    }
+}
