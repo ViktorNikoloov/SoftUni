e0fed0b338fb726359a1c7e50ee3157cca18d34e
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-25 16:15:06 +0300

    Update Multidimensional Arrays lab

---
+++ Homeworks/03.Advanced/03.MultidimensionalArraysLab/06.JaggedArrayModification/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06.JaggedArrayModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[n][];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+            }
+        }
+    }
+}
