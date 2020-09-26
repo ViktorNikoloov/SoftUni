347ca589a93cf28601a5e239f280170276a6057b
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-27 00:57:05 +0300

    Add new task to Multidimensional Arrays lab

--- Homeworks/03.Advanced/03.MultidimensionalArraysLab/06.JaggedArrayModification/Program.cs
+++ Homeworks/03.Advanced/03.MultidimensionalArraysLab/06.JaggedArrayModification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06.JaggedArrayModification
 {
@@ -8,12 +9,69 @@ namespace _06.JaggedArrayModification
         {
 
             int n = int.Parse(Console.ReadLine());
-            int[][] matrix = new int[n][];
+            int[][] jagget = new int[n][];
 
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            for (int row = 0; row < jagget.Length; row++)
             {
-
+                int[] rowData = ReadIntArray(" ");
+                jagget[row] = new int[rowData.Length];
+                for (int col = 0; col < jagget[row].Length; col++)
+                {
+                    jagget[row][col] = rowData[col];
+                }
             }
+
+            string[] command = Console.ReadLine().Split();
+            while (!command.Contains("END"))
+            {
+                string action = command[0];
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+
+                if ((0 <= row && row < jagget.Length) && (0 <= col && col <= jagget[row].Length)) 
+                {
+                    switch (command[0])
+                    {
+                        case "Add":
+                            jagget[row][col] += value;
+                            break;
+
+                        case "Subtract":
+                            jagget[row][col] -= value;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            for (int row = 0; row < jagget.Length; row++)
+            {
+                for (int col = 0; col < jagget[row].Length; col++)
+                {
+                    Console.Write(jagget[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            //foreach (var row in jagget)
+            //{
+            //    Console.WriteLine(string.Join(" ", row));
+            //}
+
         }
+
+        static int[] ReadIntArray(string separator)
+        {
+            int[] output = Console.ReadLine().Split($"{separator}", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            return output;
+        }
+
     }
 }
