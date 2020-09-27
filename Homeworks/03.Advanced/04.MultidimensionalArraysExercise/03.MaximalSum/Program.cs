b4959312b90e29b1a09f3b91a80824810b658a9e
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-28 00:26:31 +0300

    Add a new task to MultidimensionalArraysExercise

---
+++ Homeworks/03.Advanced/04.MultidimensionalArraysExercise/03.MaximalSum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = ReadIntArray(" ");
+            int[,] matrix = new int[sizes[0], sizes[1]];
+            
+            int maxSum = 0;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = ReadIntArray(" ");
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int sum = 0;
+                    sum += matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                        + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxCol = col;
+
+                    }
+                }
+            }
+
+
+            Console.WriteLine(@$"Sum = {maxSum}
+{matrix[maxRow, maxCol]} {matrix[maxRow, maxCol + 1]} {matrix[maxRow, maxCol + 2]}
+{matrix[maxRow + 1, maxCol]} {matrix[maxRow + 1, maxCol + 1]} {matrix[maxRow + 1, maxCol + 2]}
+{matrix[maxRow + 2, maxCol]} {matrix[maxRow + 2, maxCol + 1]} {matrix[maxRow + 2, maxCol + 2]}");
+        }
+
+        static int[] ReadIntArray(string separator)
+        {
+            int[] output = Console.ReadLine().Split($"{separator}", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            return output;
+        }
+    }
+}
