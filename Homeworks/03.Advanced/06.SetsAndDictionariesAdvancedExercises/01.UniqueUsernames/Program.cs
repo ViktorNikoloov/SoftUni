9e3cbfad67eb1e15a6f84d34b09875915237b29a
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-29 13:23:43 +0300

    Add new tasks to Sets And Dictionaries Advanced Exercises

---
+++ Homeworks/03.Advanced/06.SetsAndDictionariesAdvancedExercises/01.UniqueUsernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                usernames.Add(name);
+            }
+
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Homeworks/03.Advanced/06.SetsAndDictionariesAdvancedExercises/02.SetsOfElements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+            HashSet<int> uniqueSet = new HashSet<int>();
+
+            int[] length = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < length[0]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstSet.Add(number);
+            }
+
+            for (int i = 0; i < length[1]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                secondSet.Add(number);
+            }
+
+            foreach (var number in firstSet)
+            {
+                if (secondSet.Contains(number))
+                {
+                    uniqueSet.Add(number);
+                }
+            }
+
+            foreach (var number in uniqueSet)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
