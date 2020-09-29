21a28afa1ae0509fca52db1782b02f53b7b9ea07
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-29 12:34:10 +0300

    Add new tasks to Sets and Dictionaries Advanced Lab

---
+++ Homeworks/03.Advanced/5.SetsAndDictionariesAdvancedLab/06.ParkingLot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carsList = new HashSet<string>();
+
+            string[] input = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries);
+            while (input[0] != "END") //
+            {
+                string direction = input[0];
+                string car = input[1];
+
+                if (input[0] == "IN")
+                {
+                    carsList.Add(car);
+                }
+                else
+                {
+                    if (carsList.Contains(car))
+                    {
+                        carsList.Remove(car);
+                    }
+                }
+
+
+
+                input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (carsList.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in carsList)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            
+        }
+    }
+}
