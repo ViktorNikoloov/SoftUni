ac6d505524888ff0623afbad157f0f04180595e1
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-09-30 23:30:06 +0300

    Add another solution for Wardrobe's task

---
+++ Homeworks/03.Advanced/06.SetsAndDictionariesAdvancedExercises/06.1.WardrobeSecondSolution/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._1.WardrobeSecondSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string currColor = command[0];
+                string[] clothes = command[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var currCloth in clothes)
+                {
+                    if (!wardrobe.ContainsKey(currColor))
+                    {
+                        wardrobe.Add(currColor, new Dictionary<string, int>());
+                        wardrobe[currColor].Add(currCloth, 1);
+                    }
+                    else if (!wardrobe[currColor].ContainsKey(currCloth))
+                    {
+                        wardrobe[currColor].Add(currCloth, 1);
+                    }
+                    else
+                    {
+                        wardrobe[currColor][currCloth]++;
+                    }
+                }
+
+            }
+
+            string[] searching = Console.ReadLine().Split().ToArray();
+            string color = searching[0];
+            string cloth = searching[1];
+
+            foreach (var colors in wardrobe)
+            {
+                Console.WriteLine($"{colors.Key} clothes:");
+
+                foreach (var clothes in colors.Value)
+                {
+                    if (colors.Key == color && clothes.Key == cloth)
+                    {
+                        Console.WriteLine($"* {clothes.Key} - {clothes.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothes.Key} - {clothes.Value}");
+                    }
+                }
+            }
+
+        }
+    }
+}
