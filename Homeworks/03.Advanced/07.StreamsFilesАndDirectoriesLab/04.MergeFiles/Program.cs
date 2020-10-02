8dc070e8b3df79b96cfc21e297dcb476f6d2867d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-02 15:41:40 +0300

    Add Merge Files task to Streams Files Аnd Directories Lab

---
+++ Homeworks/03.Advanced/07.StreamsFilesАndDirectoriesLab/04.MergeFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader fileOneReader = new StreamReader("../../../FileOne.txt"))
+            {
+                using (StreamReader fileTwoReader = new StreamReader("../../../FileTwo.txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                    {
+                        string fileOneLine = fileOneReader.ReadLine();
+                        string fileTwoLine = fileTwoReader.ReadLine();
+
+                        while (fileOneLine != null && fileTwoLine != null)
+                        {
+                            writer.WriteLine(fileOneLine);
+                            writer.WriteLine(fileTwoLine);
+
+                            fileOneLine = fileOneReader.ReadLine();
+                            fileTwoLine = fileTwoReader.ReadLine();
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
