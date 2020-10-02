003d16a5ae3c0df1364d5e1080ea667a940a326b
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-02 15:28:19 +0300

    Add Word Count task to Streams Files Аnd Directories Lab

---
+++ Homeworks/03.Advanced/07.StreamsFilesАndDirectoriesLab/03.WordCount/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordsToFind = new Dictionary<string, int>();
+
+            using (StreamReader wordsReader = new StreamReader("../../../words.txt"))
+            {
+                while (!wordsReader.EndOfStream)
+                {
+                    string[] words = wordsReader.ReadLine().ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var line in words)
+                    {
+                        if (!wordsToFind.ContainsKey(line))
+                        {
+                            wordsToFind.Add(line, 0);
+                        }
+
+                    }
+                }
+
+            }
+
+            using (StreamReader textReader = new StreamReader("../../../text.txt"))
+            {
+                while (!textReader.EndOfStream)
+                {
+                    string[] lines = textReader.ReadLine().ToLower().Split(new char[] { '-', ',', '.', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var word in lines)
+                    {
+                        if (wordsToFind.ContainsKey(word))
+                        {
+                            wordsToFind[word]++;
+                        }
+                    }
+
+                }
+            }
+            var sortedWords = wordsToFind.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
+
+            using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+            {
+                foreach (var word in sortedWords)
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+
+        }
+    }
+}
