ef6dbef44268c0c4dae1468e9a7158426419ef19
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-05 01:18:04 +0300

    Add a new project and the first task to it

---
+++ Homeworks/03.Advanced/08.StreamsFilesАndDirectoriesExercise/01.EvenLines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+                int count = 0;
+
+                while (line != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        Regex pattern = new Regex(@"[\-\,\.\!\?]");
+                        var newLine = pattern.Replace(line, "@").Split().ToArray().Reverse();
+
+                        Console.WriteLine(string.Join(" ", newLine));
+                    }
+                    count++;
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
