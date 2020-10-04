8e8522a33d519d4a1640ad8cf5f4c73406c4cb66
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-04 23:54:06 +0300

    Add Folder Size task to Streams Files Аnd Directories Lab

---
+++ Homeworks/03.Advanced/07.StreamsFilesАndDirectoriesLab/06.FolderSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _06.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = Console.ReadLine();
+            string[] files = Directory.GetFiles(directoryPath);
+            decimal size = 0;
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo info = new FileInfo(files[i]);
+                size += info.Length;
+            }
+            string strSize = $"{size / 1024 / 1024:f14}";
+
+            File.WriteAllText("../../../Output.txt", strSize);
+
+        }
+    }
+}
