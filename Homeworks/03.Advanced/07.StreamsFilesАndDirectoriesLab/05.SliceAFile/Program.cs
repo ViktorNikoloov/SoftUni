b5199852ff0ec0dcc9885247cd2c2d998fb8b623
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-04 22:57:56 +0300

    Add Slice a File task to Streams Files Аnd Directories Lab

---
+++ Homeworks/03.Advanced/07.StreamsFilesАndDirectoriesLab/05.SliceAFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _05.SliceAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream stream = new FileStream("../../../sliceMe.txt", FileMode.Open))
+            {
+                long size = stream.Length / 4;
+                
+                for (int i = 0; i < 4; i++)
+                {
+                    using (FileStream pieceStream = new FileStream($"../../../part-{i+1}.txt",FileMode.Create))
+                    {
+                        byte[] buffer = new byte[size];
+                        int count = 0;
+
+                        while (count < size)
+                        {
+                            stream.Read(buffer, 0, buffer.Length);
+                            pieceStream.Write(buffer, 0, buffer.Length);
+
+                            count+= buffer.Length;
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
