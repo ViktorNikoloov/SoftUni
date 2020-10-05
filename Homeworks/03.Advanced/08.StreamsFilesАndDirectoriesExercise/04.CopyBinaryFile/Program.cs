289556a10d94f00f7e86d09e2a7aa202a6d70589
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-05 14:32:33 +0300

    Add Copy Binary File and update Stream Files

---
+++ Homeworks/03.Advanced/08.StreamsFilesАndDirectoriesExercise/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream CopyAFile = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (FileStream WriteAFile = new FileStream("../../../copyMe(1).png", FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+                    int count = 0;
+                    while (count < CopyAFile.Length)
+                    {
+                        CopyAFile.Read(buffer, 0, buffer.Length);
+                        WriteAFile.Write(buffer, 0, buffer.Length);
+                        count += buffer.Length;
+
+                    }
+                }
+            }
+        }
+    }
+}
