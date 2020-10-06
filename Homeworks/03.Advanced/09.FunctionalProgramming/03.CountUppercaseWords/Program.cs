e66c9e56011b9decf7022a84534c552971614e75
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-07 02:04:32 +0300

    Add some updates to FunctionalProgramming solution

--- Homeworks/03.Advanced/09.FunctionalProgramming/03.CountUppercaseWords/Program.cs
+++ Homeworks/03.Advanced/09.FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -8,6 +8,12 @@ namespace _03.CountUppercaseWords
     {
         static void Main(string[] args)
         {
+            //string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(s => s[0] == s.ToUpper()[0]).ToArray();
+            //foreach (var word in input)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
             Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(s => s[0] == s.ToUpper()[0]).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
--- Homeworks/03.Advanced/09.FunctionalProgramming/04.AddVAT/Program.cs
+++ Homeworks/03.Advanced/09.FunctionalProgramming/04.AddVAT/Program.cs
@@ -7,16 +7,22 @@ namespace _04.AddVAT
     {
         static void Main(string[] args)
         {
-            double[] arrOfDoubleNumbers = Console.ReadLine()
+            //double[] arrOfDoubleNumbers = Console.ReadLine()
+            //    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(x => double.Parse(x))
+            //    .Select(x=> x + x *0.2)
+            //    .ToArray();
+
+            //foreach (var number in arrOfDoubleNumbers)
+            //{
+            //    Console.WriteLine($"{number:f2}");
+            //}
+
+            Console.ReadLine()
                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(x => double.Parse(x))
-                .Select(x=> x + x *0.2)
-                .ToArray();
-
-            foreach (var number in arrOfDoubleNumbers)
-            {
-                Console.WriteLine($"{number:f2}");
-            }
+                .Select(x => x + x * 0.2)
+                .ToList().ForEach(x => Console.WriteLine($"{x:f2}"));
         }
     }
 }
