79d1621dd61c21748780e056dc9ca34bd67eae18
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-07 01:45:51 +0300

    Add new task to FunctionalProgramming solution

---
+++ Homeworks/03.Advanced/09.FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split().Where(s => s[0] == s.ToUpper()[0]).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
