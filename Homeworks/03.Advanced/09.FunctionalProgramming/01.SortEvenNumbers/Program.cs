973b7908e6099260b6636252ecceeb666688828b
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-06 16:07:19 +0300

    Add new solution and SortEvenNumbers task

---
+++ Homeworks/03.Advanced/09.FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x=>x).ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
