e91a9865cfb3ad6da467e72876355887f3584fde
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-06 15:53:09 +0300

    Add tasks and updates on StreamsFilesAndDirectoriesExercise solution

---
+++ Homeworks/03.Advanced/08.StreamsFilesАndDirectoriesExercise/05.DirectoryTraversal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, please enter a path: ");
+            var path = Console.ReadLine(); //Do not enter a path to an open VS's solution !
+
+            Dictionary<string, Dictionary<string, double>> filesInfo = new Dictionary<string, Dictionary<string, double>>();
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            FileInfo[] directoryFiles = directoryInfo.GetFiles();
+
+            foreach (var file in directoryFiles)
+            {
+                if (!filesInfo.ContainsKey(file.Extension))
+                {
+                    filesInfo.Add(file.Extension, new Dictionary<string, double>());
+                }
+
+                filesInfo[file.Extension].Add(file.Name, file.Length / 1000.00);
+            }
+
+            var OrderedFilesInfo = filesInfo.OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ThenByDescending(x => x.Value.Values)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            using (StreamWriter writer = new StreamWriter(@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\DirectoryTraversal.txt"))
+            {
+                foreach (var file in OrderedFilesInfo)
+                {
+                    writer.WriteLine(file.Key);
+                    foreach (var item in file.Value)
+                    {
+                        writer.WriteLine($"--{item.Key} - {item.Value}kb");
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Homeworks/03.Advanced/08.StreamsFilesАndDirectoriesExercise/6.ZipAndExtract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _6.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter a filePath: ");
+            string filePath = Console.ReadLine();
+            string[] fileName = filePath.Split("\\");
+
+            ZipFile.CreateFromDirectory(filePath, @$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\{fileName[fileName.Length -1]}.zip");
+            ZipFile.ExtractToDirectory(@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\{fileName[fileName.Length - 1]}.zip",
+                @$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\{fileName[fileName.Length - 1]}");
+        }
+    }
+}
