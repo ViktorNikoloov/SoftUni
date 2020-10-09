00978d6d772852dd73eae99810b352f9d35f40fa
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-09 09:34:13 +0300

    Add a new solution DefiningClassesLab with five task in one project

---
+++ Homeworks/03.Advanced/11.DefiningClassesLab/CarManufacturer/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+        
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+    }
+    
+}
