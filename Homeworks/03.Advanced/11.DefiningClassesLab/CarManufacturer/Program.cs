33462dcf10c9fd8f046431aaa3df3384090802cb
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-09 09:13:10 +0300

    q
    git status
    bot
    modified:   hello.html

---
+++ Homeworks/03.Advanced/11.DefiningClassesLab/CarManufacturer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(new string('-', 20));
+            //Console.WriteLine("1st and 2nd tasks");
+            //Console.WriteLine(new string('-', 20));
+
+            //Car car = new Car();
+
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+            //car.FuelQuantity = 500;
+            //car.FuelConsumption = 7;
+            //car.Drive(50);
+
+            //Console.WriteLine(car.WhoAmI());
+
+            //Console.WriteLine(new string('-', 20));
+            //Console.WriteLine("3th task");
+            //Console.WriteLine(new string('-', 20));
+
+            //List<Car> testCar = new List<Car>();
+
+            //string make = Console.ReadLine();
+            //string model = Console.ReadLine();
+            //int year = int.Parse(Console.ReadLine());
+            //double fuelQuantity = double.Parse(Console.ReadLine());
+            //double fuelConsumotion = double.Parse(Console.ReadLine());
+
+            //Car firstCar = new Car();
+            //firstCar.Drive(2);
+            //testCar.Add(firstCar);
+
+            //Car secondCar = new Car(make, model, year);
+            //secondCar.Drive(2);
+            //testCar.Add(secondCar);
+
+            //Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumotion);
+            //thirdCar.Drive(2);
+            //testCar.Add(thirdCar);
+
+            //foreach (var test in testCar)
+            //{
+            //    Console.WriteLine(test.WhoAmI());
+            //    Console.WriteLine(new string('-', 20));
+            //}
+
+            //Console.WriteLine(new string('-', 20));
+            //Console.WriteLine("4th task");
+            //Console.WriteLine(new string('-', 20));
+
+            //Tire[] tires = new Tire[]
+            //{
+            //    new Tire(1, 2.5),
+            //    new Tire(1, 2.1),
+            //    new Tire(2, 0.5),
+            //    new Tire(2, 2.3),
+            //};
+
+            //Engine engine = new Engine(560, 6300);
+            //Car cars = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+
+            //Console.WriteLine($"Make: {cars.Make}\nModel: {cars.Model}\nYear: {cars.Year}\nFuel: {cars.FuelQuantity:F2}L\nEngine:\n-HorsePower: {cars.Engine.HorsePower}\n-CubicCapacity: {cars.Engine.CubicCapacity}");
+
+            //int tiresCounter = 0;
+            //foreach (var tire in tires)
+            //{
+            //    tiresCounter++;
+
+            //    Console.WriteLine($"Tires({tiresCounter}):");
+            //    Console.WriteLine($"-Pressure: {tire.Pressure}");
+            //    Console.WriteLine($"-Year: {tire.Year}");
+
+            //    if (tiresCounter == tires.Length)
+            //    {
+            //        tiresCounter = 0;
+            //    }
+            //}
+        }
+    }
+}
