1537438047f04f5ae4dc092c9b38e85dbf492d71
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-10 23:32:09 +0300

    Add a new solution DefiningClassesExercise

---
+++ Homeworks/03.Advanced/12.DefiningClassesExercise/DefiningClasses/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person ivan = new Person();
+            
+            Person viktor = new Person(23);
+            Person tanya = new Person("tanya", 26);
+            
+            Console.WriteLine($"Name: {ivan.Name}\nAge: {ivan.Age}");
+            Console.WriteLine($"Name: {viktor.Name}\nAge: {viktor.Age}");
+            Console.WriteLine($"Name: {tanya.Name}\nAge: {tanya.Age}");
+        }
+    }
+}
