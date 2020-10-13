d751a7f307e4e63ad1830b286f2f8c7dda10131e
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-14 00:29:24 +0300

    Add new project in DefiningClassesExercise solution

---
+++ Homeworks/03.Advanced/12.DefiningClassesExercise/SoftUniParking/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+       
+
+    }
+}
