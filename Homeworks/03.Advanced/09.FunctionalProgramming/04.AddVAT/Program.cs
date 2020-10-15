b59579fb4124657f2071ca8c64d7d8ad0be37281
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-16 00:47:45 +0300

    Update FunctionProgramming solution

--- Homeworks/03.Advanced/09.FunctionalProgramming/04.AddVAT/Program.cs
+++ Homeworks/03.Advanced/09.FunctionalProgramming/04.AddVAT/Program.cs
@@ -23,6 +23,11 @@ namespace _04.AddVAT
                 .Select(x => double.Parse(x))
                 .Select(x => x + x * 0.2)
                 .ToList().ForEach(x => Console.WriteLine($"{x:f2}"));
+
+            double[] arr = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).Select(x => x + x * 0.2).ToArray();
+            Console.WriteLine(string.Join(" ", arr));
         }
+
+
     }
 }
