4e6bc4c6aa0e742da3cff645ad79d9480372b0c6
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-16 01:38:21 +0300

    Add FindEvensOrOdds project to FunctionalProgrammingExercise solution

---
+++ Homeworks/03.Advanced/10.FunctionalProgrammingExercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string evenOrOdd = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+
+            int startNum = input[0];
+            int endNum = input[1];
+            for (int i = startNum; i < endNum; i++)
+            {
+                numbers.Add(i);
+            }
+
+
+
+            Predicate<int> isEven = num => num % 2 == 0;
+
+            Action<List<int>> printNumbers = num => Console.WriteLine(string.Join(" ", num));
+
+            if (evenOrOdd == "odd")
+            {
+                numbers.RemoveAll(x => !isEven(x));
+            }
+            else
+            {
+                numbers.RemoveAll(x => isEven(x));
+
+            }
+
+            printNumbers(numbers);
+
+        }
+
+    }
+}
