9b54b6d0ceab900f34c3ca5b550105a1c69d19c6
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-18 15:12:30 +0300

    Add EqualityScale project to GenericsLab solution

---
+++ Homeworks/03.Advanced/15.GenericsLab/GenericScale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T right;
+        private T left;
+
+        public EqualityScale(T right, T left)
+        {
+            this.right = right;
+            this.left = left;
+        }
+
+        public bool AreEqual()
+        => right.Equals(left);
+            
+        
+    }
+}
---
+++ Homeworks/03.Advanced/15.GenericsLab/GenericScale/StartUp .cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> intInput = new EqualityScale<int>(5, 5);
+            Console.WriteLine(intInput.AreEqual());
+
+
+            EqualityScale<string> strInput = new EqualityScale<string>("ASD", "ASS");
+            Console.WriteLine(strInput.AreEqual());
+
+        }
+    }
+}
