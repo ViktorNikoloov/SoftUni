672b69f437396624ffe9db63ac8e081dd204e343
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-18 14:32:09 +0300

    Add new projects to GenericsLab solution

---
+++ Homeworks/03.Advanced/15.GenericsLab/BoxOfT/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            stack = new Stack<T>();
+        }
+
+        private Stack<T> stack;
+
+        public int Count
+        => stack.Count;
+        //{
+        //    get
+        //    {
+        //        return stack.Count;
+        //    }
+        //}
+
+        public void Add(T element)
+        => stack.Push(element);
+        
+
+        public T Remove()
+        => stack.Pop();
+
+        public void Print()
+        => Console.WriteLine(string.Join(", ", stack));
+        
+
+    }
+}
