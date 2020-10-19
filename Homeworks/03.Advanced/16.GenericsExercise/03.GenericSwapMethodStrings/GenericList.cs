0798873d2dff1f14fdfee2e15d467af0af333d1b
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-19 04:33:10 +0300

    Add a GenericSwapMethodStrings project to GenericsExercise solution

---
+++ Homeworks/03.Advanced/16.GenericsExercise/03.GenericSwapMethodStrings/GenericList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodStrings
+{
+    public class GenericList<T>
+    {
+        public GenericList(List<T> list)
+        {
+            List = list;
+        }
+
+        public List<T> List { get; set; } = new List<T>();
+
+        public void SwapIndexes(int firstIndex, int secondIndex)
+        {
+            T tempValue = List[firstIndex];
+            List[firstIndex] = List[secondIndex];
+            List[secondIndex] = tempValue;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var str in List)
+            {
+                sb.AppendLine($"{str.GetType()}: {str}");
+            }
+
+           return sb.ToString().Trim();
+
+        }
+
+
+
+    }
+}
---
+++ Homeworks/03.Advanced/16.GenericsExercise/03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            string[] swapCommand = Console.ReadLine().Split();
+            int firstIndex = int.Parse(swapCommand[0]);
+            int secondIndex = int.Parse(swapCommand[1]);
+
+            GenericList<string> genericList = new GenericList<string>(list);
+            genericList.SwapIndexes(firstIndex, secondIndex);
+
+            Console.WriteLine(genericList.ToString());
+         
+            
+
+        }
+    }
+}
