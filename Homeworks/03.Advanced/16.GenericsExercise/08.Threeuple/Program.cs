f74aba188309a6f6dd47a6db4a68765335f6b241
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-21 01:43:21 +0300

    Add Threeuple project to GenericsExercise solution

---
+++ Homeworks/03.Advanced/16.GenericsExercise/08.Threeuple/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAndAddress = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string fullName = $"{nameAndAddress[0]} {nameAndAddress[1]}";
+            string address = nameAndAddress[2];
+            var town = string.Join(" ", nameAndAddress.Skip(3));
+
+            Threeuple<string, string, string> strTuple = new Threeuple<string, string, string>(fullName, address, town);
+            Console.WriteLine(strTuple);
+
+            //-------------------------------------------------------------------------------------------------
+
+            string[] nameBeersCountAndCondition = Console.ReadLine().Split();
+
+            string name = nameBeersCountAndCondition[0];
+            int litersOfBeer = int.Parse(nameBeersCountAndCondition[1]);
+            bool isDrunk = nameBeersCountAndCondition[2] == "drunk" ? true : false;
+
+            Threeuple<string, int, bool> strAndIntTuple = new Threeuple<string, int, bool>(name, litersOfBeer, isDrunk);
+            Console.WriteLine(strAndIntTuple);
+
+            //-------------------------------------------------------------------------------------------------
+
+
+            string[] numbersInput = Console.ReadLine().Split();
+
+            string firstName = numbersInput[0];
+            double accountBalance = double.Parse(numbersInput[1]);
+            string bankName = numbersInput[2];
+
+
+            Threeuple<string, double, string> integerAndDoubleTuple = new Threeuple<string, double, string>(firstName, accountBalance, bankName);
+            Console.WriteLine(integerAndDoubleTuple);
+        }
+    }
+}
---
+++ Homeworks/03.Advanced/16.GenericsExercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public Threeuple(T1 firstvalue, T2 secondvalue, T3 thirdvalue)
+        {
+            FirstValue = firstvalue;
+            SecondValue = secondvalue;
+            ThirdValue = thirdvalue;
+        }
+
+        public T1 FirstValue { get; set; }
+
+        public T2 SecondValue { get; set; }
+
+        public T3 ThirdValue { get; set; }
+
+        public override string ToString()
+        => $"{FirstValue} -> {SecondValue} -> {ThirdValue}";
+    }
+}
