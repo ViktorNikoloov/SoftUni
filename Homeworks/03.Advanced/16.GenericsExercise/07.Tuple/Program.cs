1c7f41002bfdbe602d3debfb393f8157699c6b0b
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-21 01:20:02 +0300

    Add Tuple project to GenericsExercise solution

---
+++ Homeworks/03.Advanced/16.GenericsExercise/07.Tuple/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAndAddress = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string fullName = $"{nameAndAddress[0]} {nameAndAddress[1]}";
+            string address = nameAndAddress[2];
+
+            Tuple<string, string> strTuple = new Tuple<string, string>(fullName, address);
+            Console.WriteLine(strTuple);
+
+            //-------------------------------------------------------------------------------------------------
+
+            string[] nameAndBeersCount = Console.ReadLine().Split();
+
+            string name = nameAndBeersCount[0];
+            int beersCount = int.Parse(nameAndBeersCount[1]);
+
+            Tuple<string, int> strAndIntTuple = new Tuple<string, int>(name, beersCount);
+            Console.WriteLine(strAndIntTuple);
+
+            //-------------------------------------------------------------------------------------------------
+
+
+            double[] numbersInput = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            int intNumber = (int)numbersInput[0];
+            double doubleNumber = numbersInput[1];
+
+            Tuple<int, double> integerAndDoubleTuple = new Tuple<int, double>(intNumber, doubleNumber);
+            Console.WriteLine(integerAndDoubleTuple);
+        }
+    }
+}
---
+++ Homeworks/03.Advanced/16.GenericsExercise/07.Tuple/Tuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<T1, T2>
+    {
+        private T1 firstItem;
+
+        private T2 secondItem;
+
+        public Tuple(T1 firstItem, T2 seconitem)
+        {
+            this.firstItem = firstItem;
+            this.secondItem = seconitem;
+        }
+
+        public override string ToString()
+        => $"{firstItem} -> {secondItem}";
+
+        //public T1 FirstItem { get; set; }
+
+        //public T2 SecondItem { get; set; }
+    }
+}
