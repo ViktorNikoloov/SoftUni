17f0f851698236fc76c50a33673551140ba4fe00
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-21 00:58:09 +0300

    Add GenericCountMethodStrings project to GenericsExercise solution

---
+++ Homeworks/03.Advanced/16.GenericsExercise/05.GenericCountMethodStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.GenericCountMethodStrings
+{
+    class Program
+    {
+        class Box<T>
+        where T : IComparable
+        {
+            public Box()
+            {
+                List = new List<T>();
+            }
+
+            public List<T> List { get; set; }
+
+            public int GetCompared(T element)
+            {
+                int counter = 0;
+
+                foreach (var value in List)
+                {
+                    if (value.CompareTo(element) > 0)
+                    {
+                        counter++;
+                    }
+                }
+
+                return counter;
+            }
+
+        }
+    }
+}
