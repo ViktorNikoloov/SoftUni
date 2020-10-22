f0be70062829decf42ca79cb24e962362549938d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-22 23:55:40 +0300

    Add 07.Lootbox22Feb2020 project to 00.ExamPreparation solution

--- Homeworks/03.Advanced/00.ExamPreparation/07.Lootbox22Feb2020/Program.cs
+++ Homeworks/03.Advanced/00.ExamPreparation/07.Lootbox22Feb2020/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.Lootbox22Feb2020
 {
@@ -6,7 +8,48 @@ namespace _07.Lootbox22Feb2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] firstBoxInput= Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondBoxInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> firstBox = new Queue<int>(firstBoxInput);
+            Stack<int> secondBox = new Stack<int>(secondBoxInput);
+            int items = 0;
+            
+            while (firstBox.Count != 0 && secondBox.Count != 0)
+            {
+                int sum = firstBox.Peek() + secondBox.Peek();
+                if (sum % 2 == 0)
+                {
+                    items += sum;
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondBox.Pop());
+                }
+
+
+            }
+
+            if (firstBox.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (items >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {items}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {items}");
+
+            }
         }
     }
 }
