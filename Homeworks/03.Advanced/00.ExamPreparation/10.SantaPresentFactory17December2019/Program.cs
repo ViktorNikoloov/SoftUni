94f25585e7ed30c07cce0534580897a71b61a805
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-23 09:31:58 +0300

    Add 10.SantaPresentFactory17December2019 and 11.PresentDelivery17December2019 empty projects in 00.ExamPreparation solution

---
+++ Homeworks/03.Advanced/00.ExamPreparation/10.SantaPresentFactory17December2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10.SantaPresentFactory17December2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Homeworks/03.Advanced/00.ExamPreparation/11.PresentDelivery17December2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11.PresentDelivery17December2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
