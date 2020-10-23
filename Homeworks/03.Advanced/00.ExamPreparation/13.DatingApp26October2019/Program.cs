40755525984a87142d15bef31092154df4638b24
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-23 12:44:40 +0300

    Add empty projects to 00.ExamPreparation solution

---
+++ Homeworks/03.Advanced/00.ExamPreparation/13.DatingApp26October2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13.DatingApp26October2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Homeworks/03.Advanced/00.ExamPreparation/14.BookWorm26October2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14.BookWorm26October2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
