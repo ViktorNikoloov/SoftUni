d0028a0574813a7e94721b3347055875ec2ff28c
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-27 22:20:01 +0200

    Add 01.SingleInheritance project to 01.Inheritance solution

---
+++ Homeworks/04.OOP/01.Inheritance/Farm/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Farm
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Eat();
+
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
