98752e7d5de69f6111557ad0c800ef34f3d25e7c
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-27 23:36:58 +0200

    Add 03.HierarchicalInheritance project to 01.Inheritance solution

---
+++ Homeworks/04.OOP/01.Inheritance/03.HierarchicalInheritance/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+        public void Meow()
+            => Console.WriteLine("meowing…");
+    }
+}
