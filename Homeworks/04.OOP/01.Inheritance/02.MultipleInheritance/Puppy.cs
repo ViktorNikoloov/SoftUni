33fb9e960fbc3c93cfa02148f799bd68fe51d8b0
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-27 23:25:16 +0200

    Add 02.MultipleInheritance project to 01.Inheritance solution

---
+++ Homeworks/04.OOP/01.Inheritance/02.MultipleInheritance/Puppy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    class Puppy : Dog
+    {
+        public void Weep()
+            => Console.WriteLine("weeping…");
+    }
+}
---
+++ Homeworks/04.OOP/01.Inheritance/02.MultipleInheritance/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Eat();
+
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Puppy puppy = new Puppy();
+
+            puppy.Weep();
+            puppy.Bark();
+            puppy.Eat();
+        }
+    }
+}
