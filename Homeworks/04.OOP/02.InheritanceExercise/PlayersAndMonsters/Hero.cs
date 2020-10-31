6d034a069721a2fa811dcdc313d5238a834446a2
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-31 23:47:35 +0200

    Add PlayersAndMonsters project to 02.InheritanceExercise solution

---
+++ Homeworks/04.OOP/02.InheritanceExercise/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public abstract class Hero
+    {
+        public Hero(string username, int level)
+        {
+            Username = username;
+            Level = level;
+        }
+
+        public string Username { get; set; }
+
+        public int Level { get; set; }
+
+        public override string ToString()
+        => $"Type: {GetType().Name} Username: {Username} Level: {Level}";
+    }
+}
