f9e578e7ee373332c6f37b45a9b25aa82c148593
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-31 17:04:18 +0200

    Add 05.StackOfStrings project to 01.Inheritance solution

---
+++ Homeworks/04.OOP/01.Inheritance/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return  this.Count == 0;
+        }
+
+        public void AddRange(Stack<string> range)
+        {
+            this.AddRange(range);
+        }
+    }
+}
