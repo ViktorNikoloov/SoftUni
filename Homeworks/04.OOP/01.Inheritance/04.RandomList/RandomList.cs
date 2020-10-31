d15b80285f89bd97aa1cc3aa6dffcaaf13b470a9
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-10-31 16:43:29 +0200

    Add 04.RandomList project to 01.Inheritance solution

---
+++ Homeworks/04.OOP/01.Inheritance/04.RandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rand;
+        public RandomList()
+        {
+            rand = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = rand.Next(0, this.Count);
+
+            string returnedStr = this[index];
+            this.RemoveAt(index);
+            return returnedStr;
+            
+
+
+        }
+    }
+}
---
+++ Homeworks/04.OOP/01.Inheritance/04.RandomList/StartUp .cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randList = new RandomList();
+
+            randList.Add("A");
+            randList.Add("B");
+            randList.Add("C");
+            randList.Add("D");
+            Console.WriteLine($"List content: {string.Join(", ", randList)}");
+
+            Console.WriteLine(new string('-', 20));
+
+
+            Console.WriteLine($"List count: {randList.Count}");
+            Console.WriteLine($"Removed string: {randList.RandomString()}");
+            Console.WriteLine($"List count: {randList.Count}");
+
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine($"List content: {string.Join(", ", randList)}");
+
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine($"List count: {randList.Count}");
+            Console.WriteLine($"Removed string: {randList.RandomString()}");
+            Console.WriteLine($"List count: {randList.Count}");
+
+            Console.WriteLine(new string('-', 20));
+
+
+            Console.WriteLine($"List content: {string.Join(", ", randList)}");
+
+        }
+    }
+}
