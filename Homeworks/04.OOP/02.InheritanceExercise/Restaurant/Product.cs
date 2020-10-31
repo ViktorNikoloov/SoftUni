2d1f2f8093fb14160af40aed1ff33d3f02fab8d0
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-11-01 00:41:32 +0200

    Add Restaurant project to 02.InheritanceExercise solution

---
+++ Homeworks/04.OOP/02.InheritanceExercise/Restaurant/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public abstract class Product
+    {
+        protected Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
