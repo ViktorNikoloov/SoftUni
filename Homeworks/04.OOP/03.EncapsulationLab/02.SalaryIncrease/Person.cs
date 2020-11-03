00d8efd5423235ed9319e835e6864d22a0864a5c
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-11-04 01:43:55 +0200

    Add 02.SalaryIncrease project to 03.EncapsulationLab solution

---
+++ Homeworks/04.OOP/03.EncapsulationLab/02.SalaryIncrease/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += Salary * (percentage/100);
+
+            }
+            else
+            {
+                Salary += Salary * (percentage/100) / 2;
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
