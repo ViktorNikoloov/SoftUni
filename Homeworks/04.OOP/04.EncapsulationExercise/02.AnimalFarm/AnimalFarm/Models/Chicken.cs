c59a175c7693f52ccc275a988c07515b72e4f744
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-11-05 01:56:27 +0200

    Add 02.AnimalFarm project to 04.EncapsulationExercise solution

---
+++ Homeworks/04.OOP/04.EncapsulationExercise/02.AnimalFarm/AnimalFarm/Models/Chicken.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+
+        private const string NAME_EXS_STRING = "Name cannot be empty.";
+        private const string AGE_EXS_STRING = "Age should be between 0 and 15.";
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(NAME_EXS_STRING);
+                }
+
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            private set
+            {
+                if (MinAge > value || value > MaxAge)
+                {
+                    throw new ArgumentException(AGE_EXS_STRING);
+                }
+                age = value;
+            }
+        }
+
+        public double ProductPerDay
+        {
+			get
+			{				
+				return CalculateProductPerDay();
+			}
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
