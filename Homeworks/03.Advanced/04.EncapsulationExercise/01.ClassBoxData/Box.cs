3d8697f599b7cc304f15f5cb616bcfd1ceb9bbfa
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-11-05 01:24:57 +0200

    Add new solution 04.EncapsulationExercise and the project 01.ClassBoxData to it

---
+++ Homeworks/03.Advanced/04.EncapsulationExercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private const string EXS_STRING_NAME = "{0} cannot be zero or negative.";
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                IsItValid(value, nameof(Length));
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                IsItValid(value, nameof(Width));
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                IsItValid(value, nameof(Height));
+
+                height = value;
+            }
+        }
+
+        public double LateralSurfaceArea() //2lh + 2wh
+        => (2 * Length * Height) + (2 * Width * Height);
+
+
+        public double SurfaceArea() //2lw + 2lh + 2wh
+        => LateralSurfaceArea() + (2 * Length * Width);
+
+        public double Volume() //lwh
+            => Length * Width * Height;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb
+            .AppendLine($"Surface Area - {SurfaceArea():f2}")
+            .AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}")
+            .AppendLine($"Volume - {Volume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+        private void IsItValid(double side, string name)
+        {
+            if (side <= 0)
+            {
+                throw new ArgumentException(string.Format(EXS_STRING_NAME, name));
+            }
+        }
+    }
+}
