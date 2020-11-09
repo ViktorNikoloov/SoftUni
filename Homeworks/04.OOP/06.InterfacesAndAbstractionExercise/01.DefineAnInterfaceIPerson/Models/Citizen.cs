8db1c4248a56f2475dff30cd252a71c4d365638c
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-11-09 03:18:54 +0200

    Add 01.DefineAnInterfaceIPerson project to 06.InterfacesAndAbstractionExercise solution

---
+++ Homeworks/04.OOP/06.InterfacesAndAbstractionExercise/01.DefineAnInterfaceIPerson/Models/Citizen.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
