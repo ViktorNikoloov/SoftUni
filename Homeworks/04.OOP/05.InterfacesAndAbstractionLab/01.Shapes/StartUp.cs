96f15e66b9e187fe89560c4ea83c0ebf9afbe749
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-11-09 02:21:12 +0200

    Add 01.Shapes project to 05.InterfacesAndAbstractionLab solution

---
+++ Homeworks/04.OOP/05.InterfacesAndAbstractionLab/01.Shapes/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int radius = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            IDrawable circle = new Circle(radius);
+            IDrawable rectangle = new Rectangle(width, height);
+
+            circle.Draw();
+            rectangle.Draw();
+
+        }
+    }
+}
