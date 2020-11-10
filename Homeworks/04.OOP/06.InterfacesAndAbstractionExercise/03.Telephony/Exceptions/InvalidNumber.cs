98eb3bd4187d37dfb2b50aca506dc74be9fd11e0
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2020-11-11 01:00:32 +0200

    Add Cumstum exceptions

---
+++ Homeworks/04.OOP/06.InterfacesAndAbstractionExercise/03.Telephony/Exceptions/InvalidNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Telephony.Exceptions
+{
+    public class InvalidNumber : Exception
+    {
+        private const string NumberExsMsg = "Invalid number!";
+
+        public InvalidNumber()
+            :base(NumberExsMsg)
+        {
+
+        }
+
+        public InvalidNumber(string message) 
+            : base(message)
+        {
+        }
+    }
+}
---
+++ Homeworks/04.OOP/06.InterfacesAndAbstractionExercise/03.Telephony/Exceptions/InvalidUrl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.Telephony.Exceptions
+{
+    public class InvalidUrl : Exception
+    {
+        private const string UrlExceptionMsg = "Invalid URL!";
+
+        public InvalidUrl()
+            :base(UrlExceptionMsg)
+        {
+
+        }
+
+        public InvalidUrl(string message) 
+            : base(message)
+        {
+
+        }
+    }
+}
