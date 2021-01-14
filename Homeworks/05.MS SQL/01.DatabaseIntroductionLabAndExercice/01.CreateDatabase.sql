7a4458f3efa0dd12dd14df944e911ff829ec1e3e
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-14 14:14:37 +0200

    Add queries

--- Homeworks/05.MS SQL/01.DatabaseIntroductionLabAndExercice/01.CreateDatabase.sql
+++ Homeworks/05.MS SQL/01.DatabaseIntroductionLabAndExercice/01.CreateDatabase.sql
@@ -1,1 +1,4 @@
 CREATE DATABASE Minions
+
+--Minions (Id, Name, Age). Then add new table Towns (Id, Name). 
+
---
+++ Homeworks/05.MS SQL/01.DatabaseIntroductionLabAndExercice/02.CreateTables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Minions
+(
+	Id INT PRIMARY KEY,
+	[Name] VARCHAR(30),
+	Age INT
+)
+
+CREATE TABLE Towns
+(
+	Id INT PRIMARY KEY,
+	[Name] VARCHAR(30)
+)
