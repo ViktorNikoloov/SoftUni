77be36c96191f26c6769e24b987288419989dcb5
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-18 02:03:46 +0200

    Add new queries to 02.CRUD project

---
+++ Homeworks/05.MS SQL/02.CRUD/06.FindEmailAddressOfEachEmployee.sql
@@ -0,0 +1,6 @@
+USE SoftUni
+GO
+
+SELECT FirstName + '.' + LastName + '@softuni.bg' 
+	AS[Full Email Address]
+	FROM Employees
---
+++ Homeworks/05.MS SQL/02.CRUD/07.FindAllDifferentEmployeesSalaries.sql
@@ -0,0 +1,5 @@
+USE SoftUni
+GO
+
+SELECT DISTINCT Salary
+	FROM Employees
