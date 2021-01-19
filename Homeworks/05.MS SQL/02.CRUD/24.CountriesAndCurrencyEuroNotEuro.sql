c40964fb714695d51f878ad0e0ba9dd2bf9dbcb4
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-20 01:16:27 +0200

    Add 24.CountriesAndCurrencyEuroNotEuro query to 02.CRUD project

---
+++ Homeworks/05.MS SQL/02.CRUD/24.CountriesAndCurrencyEuroNotEuro.sql
@@ -0,0 +1,10 @@
+USE Geography
+GO	
+
+SELECT CountryName, CountryCode,
+	CASE
+		WHEN CurrencyCode = 'EUR' THEN 'Euro'
+		ELSE 'Not Euro'
+		END AS [Currency]
+	FROM Countries
+	ORDER BY CountryName ASC
