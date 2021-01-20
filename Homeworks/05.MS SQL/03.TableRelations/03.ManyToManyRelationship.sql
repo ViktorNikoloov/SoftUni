13dd1e509bc5291b915d9c1ff759301709c5131d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-21 00:24:40 +0200

    Add modify queries

---
+++ Homeworks/05.MS SQL/03.TableRelations/03.ManyToManyRelationship.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Students
+(
+	StudentID INT IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(50)
+)
+
+INSERT INTO Students VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+CREATE TABLE Exams
+(
+	ExamID INT IDENTITY(101,1) PRIMARY KEY,
+	[Name] NVARCHAR(50)
+)
+
+INSERT INTO Exams VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT NOT NULL
+	FOREIGN KEY REFERENCES Students(StudentID),
+	ExamID INT NOT NULL
+	FOREIGN KEY REFERENCES Exams(ExamID)
+	CONSTRAINT PK_StudentsExams
+	PRIMARY KEY(StudentID, ExamID)
+)
