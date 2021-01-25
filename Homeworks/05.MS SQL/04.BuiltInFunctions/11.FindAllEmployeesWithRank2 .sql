1ac306307f0b87b30e6ea4928e3fee71623f6992
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-26 00:15:49 +0200

    Add new queries to 04.BuiltInFunctions project

---
+++ Homeworks/05.MS SQL/04.BuiltInFunctions/11.FindAllEmployeesWithRank2 .sql
@@ -0,0 +1,21 @@
+USE SoftUni
+GO
+
+SELECT * 
+	FROM (
+		 SELECT
+				EmployeeID, 
+				FirstName, 
+				LastName, 
+				Salary,
+				DENSE_RANK() OVER 
+				(
+					PARTITION BY Salary 
+					ORDER BY EmployeeID
+				) AS [Rank]
+	FROM Employees
+		 ) AS RankList
+WHERE Salary BETWEEN 10000 AND 50000
+	AND [Rank] = 2
+ORDER BY Salary DESC
+		
---
+++ Homeworks/05.MS SQL/04.BuiltInFunctions/12.CountriesHolding‘A’3OrMoreTimes.sql
@@ -0,0 +1,7 @@
+USE Geography
+GO
+
+SELECT CountryName, IsoCode
+	FROM Countries
+	WHERE CountryName LIKE ('%A%A%A%')
+	ORDER BY IsoCode
