e5753db8796937c69044ca57cb91a963a54e439e
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-30 04:07:15 +0200

    Modify 18.HighestPeakNameAndElevationByCountry

--- Homeworks/05.MS SQL/05.SubqueriesAndJOINs/18.HighestPeakNameAndElevationByCountry.sql
+++ Homeworks/05.MS SQL/05.SubqueriesAndJOINs/18.HighestPeakNameAndElevationByCountry.sql
@@ -3,32 +3,32 @@ GO
 
 SELECT TOP(5) Country, 
 			  CASE
-			  		WHEN PeakName IS NOT NULL THEN PeakName
-			  		WHEN PeakName IS NULL THEN '(no highest peak)'
+			  	  WHEN PeakName IS NULL THEN '(no highest peak)'
+				  ELSE PeakName
 			  END AS [Highest Peak Name],
 			  CASE
-			  		WHEN Elevation IS NOT NULL THEN Elevation
-			  		WHEN PeakName IS NULL THEN 0
+			  	  WHEN Elevation IS NULL THEN 0
+				  ELSE Elevation
 			  END AS [Highest Peak Elevation],
 			  CASE
-			  		WHEN MountainRange IS NOT NULL THEN MountainRange
-			  		WHEN PeakName IS NULL THEN '(no mountain)'
+			  	  WHEN MountainRange IS NULL THEN '(no mountain)'
+				  ELSE MountainRange
 			  END AS [Mountain]	
 FROM (
-		SELECT c.CountryName AS Country, 
-			   m.MountainRange,
-		       p.PeakName,
-			   p.Elevation,
-			   DENSE_RANK() 		
+	  SELECT c.CountryName AS Country, 
+			 m.MountainRange,
+		     p.PeakName,
+			 p.Elevation,
+			 DENSE_RANK() 		
 				OVER (
-						 PARTITION BY CountryName
-						 ORDER BY p.Elevation DESC
+					  PARTITION BY CountryName
+					  ORDER BY p.Elevation DESC
 				     ) AS [ElevationRank]
-		FROM Countries AS c
-		LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
-		LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
-		LEFT JOIN Peaks AS p ON m.id = p.MountainId
+	  FROM Countries AS c
+	  LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+	  LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
+	  LEFT JOIN Peaks AS p ON m.id = p.MountainId
      ) AS [ElevationRankQuery]
 WHERE [ElevationRank] = 1
-ORDER BY CountryName ASC,
+ORDER BY Country ASC,
 [Highest Peak Name] ASC
