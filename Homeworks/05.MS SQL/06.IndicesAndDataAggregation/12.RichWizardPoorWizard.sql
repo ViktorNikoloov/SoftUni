9611dc2220594c9d8a25503e04991960ddc3b4b0
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-02-01 12:03:29 +0200

    Add new queries to IndicesAndDataAggregation project

---
+++ Homeworks/05.MS SQL/06.IndicesAndDataAggregation/12.RichWizardPoorWizard.sql
@@ -0,0 +1,7 @@
+USE Gringotts
+GO
+
+SELECT *
+	FROM WizzardDeposits
+
+--Mr. Bodrog definitely likes his werewolves more than you. This is your last chance to survive! Give him some data to play his favorite game Rich Wizard, Poor Wizard. The rules are simple: You compare the deposits of every wizard with the wizard after him. If a wizard is the last one in the database, simply ignore it. In the end you have to sum the difference between the deposits.
---
+++ Homeworks/05.MS SQL/06.IndicesAndDataAggregation/13.DepartmentsTotalSalaries.sql
@@ -0,0 +1,7 @@
+USE SoftUni
+GO
+
+SELECT DepartmentID, SUM(Salary) AS [TotalSalary]
+	FROM Employees
+	GROUP BY DepartmentID
+	ORDER BY DepartmentID ASC
