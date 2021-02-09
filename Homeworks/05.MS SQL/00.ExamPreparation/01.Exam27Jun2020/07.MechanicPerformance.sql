0c8e46ccd2ea7e7bacd1aaedbcfa0bc45b2ceb1d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-02-09 16:52:23 +0200

    Add 07.MechanicPerformance query to 01.Exam27Jun2020 project

---
+++ Homeworks/05.MS SQL/00.ExamPreparation/01.Exam27Jun2020/07.MechanicPerformance.sql
@@ -0,0 +1,10 @@
+USE WMS
+GO
+
+SELECT CONCAT(m.FirstName, ' ', m.LastName) AS [Mechanic],
+	   AVG(DATEDIFF(DAY, j.IssueDate, j.FinishDate)) AS [Average Days]
+	FROM Mechanics AS m
+	JOIN Jobs AS j ON m.MechanicId = j.MechanicId
+WHERE FinishDate IS NOT NULL
+GROUP BY j.MechanicId, m.FirstName, m.LastName
+ORDER BY m.MechanicId ASC
