d63a56b07be41136de307d7de724404eab96fb19
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-02-12 21:08:31 +0200

    Fix 08.DeleteEmployeesAndDepartments query with working code

--- Homeworks/05.MS SQL/07.FunctionsAndStoredProcedures/08.DeleteEmployeesAndDepartments.sql
+++ Homeworks/05.MS SQL/07.FunctionsAndStoredProcedures/08.DeleteEmployeesAndDepartments.sql
@@ -4,37 +4,29 @@ GO
 CREATE PROCEDURE usp_DeleteEmployeesFromDepartment(@DepartmentId INT)
 AS
 BEGIN
-	--Delete all records from EmployeesProjects where EmployeeId is going to be deleted
-	DELETE
-		FROM EmployeesProjects
+	
+	DELETE FROM EmployeesProjects
 		WHERE EmployeeID IN	(
 							 SELECT EmployeeID
 							 FROM Employees
 							 WHERE DepartmentID = @DepartmentId
 						    )
 
-    --Set ManagerId to null where Manager is an Employee who is going to be deleted
 	UPDATE Employees
 		SET ManagerID = NULL
-		WHERE EmployeeID IN (
+		WHERE ManagerID IN (
 							 SELECT EmployeeID
 							 FROM Employees
 							 WHERE DepartmentID = @DepartmentId
 							)
 
-	--Set ManagerID column in Departments table to nullable 
 	ALTER TABLE Departments
 	ALTER COLUMN ManagerID INT
 
 	UPDATE Departments
 		SET ManagerID = NULL
-		WHERE ManagerID IN (
-							SELECT ManagerID
-							FROM Employees
-							WHERE DepartmentID = @DepartmentId
-						   )
-
-	--Deletes all Employees from a given department				
+		WHERE DepartmentID = @DepartmentId
+			
 	DELETE FROM Employees
 	WHERE DepartmentID = @DepartmentId
 
