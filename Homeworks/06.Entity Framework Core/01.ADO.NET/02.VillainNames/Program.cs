9ca8cea8bfb1df5c4f90f6b2c19f36847ffd894e
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-02-17 02:35:28 +0200

    Add 02.VillainNames project to 01.ADO.NET solution

---
+++ Homeworks/06.Entity Framework Core/01.ADO.NET/02.VillainNames/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _02.VillainNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("Server=.\\SQLEXPRESS;Database=MinionsDB;Integrated Security=true;"))
+            {
+                sqlConnection.Open();
+                string query = 
+                    "SELECT v.Name, " +
+                    "COUNT(mv.VillainId) AS MinionsCount  " +
+                        "FROM Villains AS v " +
+                        "JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                    "GROUP BY v.Id, v.Name HAVING COUNT(mv.VillainId) > 3 " +
+                    "ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand command = new SqlCommand(query, sqlConnection);
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine($"{sqlDataReader["Name"]} - {sqlDataReader["MinionsCount"]}");
+                }
+            }
+        }
+    }
+}
