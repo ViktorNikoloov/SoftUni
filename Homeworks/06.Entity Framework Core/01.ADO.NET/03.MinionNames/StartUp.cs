8325469b7c8344fe0554970482f72b866dafe4fe
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-02-17 03:10:27 +0200

    Add 03.MinionNames project to 01.ADO.NET solution

---
+++ Homeworks/06.Entity Framework Core/01.ADO.NET/03.MinionNames/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _03.MinionNames
+{
+    class StartUp
+    {
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=MinionsDB;Integrated Security=true;";
+
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+
+                string villainId = Console.ReadLine();
+
+                SqlCommand command = new SqlCommand($"SELECT Name FROM Villains WHERE Id = {villainId}", sqlConnection);
+
+
+            }
+
+        }
+    }
+}
