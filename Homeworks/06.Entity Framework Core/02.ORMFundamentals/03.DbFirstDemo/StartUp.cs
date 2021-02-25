6a2720d67d0d3da42aeba46534b979c4aab98729
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-02-25 03:30:21 +0200

    Add 03.DbFirstDemo project to 02.ORMFundamentals

---
+++ Homeworks/06.Entity Framework Core/02.ORMFundamentals/03.DbFirstDemo/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.DbFirstDemo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
