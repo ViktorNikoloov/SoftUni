5f0927b5f204eb00ed449ee3bc3a4b3beed2610a
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-02-26 03:41:57 +0200

    Add 02.DatabaseFirst project to 04.EntityFrameworkIntroduction solution

---
+++ Homeworks/06.Entity Framework Core/04.EntityFrameworkIntroduction/02.DatabaseFirst/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Install-Package Microsoft.EntityFrameworkCore.Tools –v 3.1.3
+            Install-Package Microsoft.EntityFrameworkCore.SqlServer –v 3.1.3
+            Install-Package Microsoft.EntityFrameworkCore.SqlServer.Design
+            */
+        }
+    }
+}
