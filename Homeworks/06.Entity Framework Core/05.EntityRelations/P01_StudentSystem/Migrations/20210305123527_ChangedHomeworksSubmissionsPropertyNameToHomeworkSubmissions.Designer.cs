4ebe59ac4454d26f4e70a3e5b6829f7ebabe819d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-05 14:36:28 +0200

    Add new migration to P01_StudentSystem project

--- Homeworks/06.Entity Framework Core/05.EntityRelations/P01_StudentSystem/Migrations/20210305123527_ChangedHomeworksSubmissionsPropertyNameToHomeworkSubmissions.Designer.cs
+++ Homeworks/06.Entity Framework Core/05.EntityRelations/P01_StudentSystem/Migrations/20210305123527_ChangedHomeworksSubmissionsPropertyNameToHomeworkSubmissions.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using P01_StudentSystem.Data;
 
 namespace P01_StudentSystem.Migrations
 {
     [DbContext(typeof(StudentSystemContext))]
-    partial class StudentSystemContextModelSnapshot : ModelSnapshot
+    [Migration("20210305123527_ChangedHomeworksSubmissionsPropertyNameToHomeworkSubmissions")]
+    partial class ChangedHomeworksSubmissionsPropertyNameToHomeworkSubmissions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
