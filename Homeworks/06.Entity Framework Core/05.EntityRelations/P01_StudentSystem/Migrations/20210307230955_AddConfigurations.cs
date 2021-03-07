73fa922c9565e6d6ab74c7dcad56cf9cf510c056
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-08 01:10:58 +0200

    Add Configurations Migration

---
+++ Homeworks/06.Entity Framework Core/05.EntityRelations/P01_StudentSystem/Migrations/20210307230955_AddConfigurations.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_StudentSystem.Migrations
+{
+    public partial class AddConfigurations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resources_Courses_CourseId",
+                table: "Resources");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_Students_StudentId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudentCourses_StudentId",
+                table: "StudentCourses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Resources",
+                unicode: false,
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "HomeworkSubmissions",
+                unicode: false,
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Courses",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses",
+                columns: new[] { "StudentId", "CourseId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentCourses_CourseId",
+                table: "StudentCourses",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resources_Courses_CourseId",
+                table: "Resources",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_Students_StudentId",
+                table: "StudentCourses",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resources_Courses_CourseId",
+                table: "Resources");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_Students_StudentId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudentCourses_CourseId",
+                table: "StudentCourses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Resources",
+                type: "varchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldUnicode: false,
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "HomeworkSubmissions",
+                type: "varchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldUnicode: false,
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses",
+                columns: new[] { "CourseId", "StudentId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentCourses_StudentId",
+                table: "StudentCourses",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resources_Courses_CourseId",
+                table: "Resources",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_Students_StudentId",
+                table: "StudentCourses",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
