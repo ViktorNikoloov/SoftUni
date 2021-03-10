38c48708868616d5d5a29b3242afc1afa8515a6a
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-10 15:24:24 +0200

    Add Configuration

---
+++ Homeworks/06.Entity Framework Core/06.LINQ/MusicHun/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConfigurationString = @"Server=.\SQLEXPRESS;Database=MusicHub;Integrated Security=true;";
+    }
+}
