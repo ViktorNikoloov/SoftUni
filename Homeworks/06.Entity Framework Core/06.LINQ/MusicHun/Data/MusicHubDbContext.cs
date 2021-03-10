87b192a15a83f9dd17c97db025abd45ef0f69c50
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-10 15:24:49 +0200

    Add MusicHubDbContext to MusicHub project

---
+++ Homeworks/06.Entity Framework Core/06.LINQ/MusicHun/Data/MusicHubDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicHub.Data
+{
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConfigurationString);
+            }
+        }
+    }
+}
