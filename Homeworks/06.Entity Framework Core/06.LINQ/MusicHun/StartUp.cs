fb413ec5bf610292e52ee096025cf1022fcbcf96
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-10 23:12:31 +0200

    Refactoring StartUp.cs

--- Homeworks/06.Entity Framework Core/06.LINQ/MusicHun/StartUp.cs
+++ Homeworks/06.Entity Framework Core/06.LINQ/MusicHun/StartUp.cs
@@ -1,8 +1,12 @@
-﻿using MusicHub.Data;
-using System;
-using System.Globalization;
-using System.Linq;
+﻿using System;
 using System.Text;
+using System.Linq;
+using System.Globalization;
+
+using Microsoft.EntityFrameworkCore;
+
+using MusicHub.Data;
+using MusicHub.Initializer;
 
 namespace MusicHub
 {
@@ -10,35 +14,39 @@ namespace MusicHub
     {
         static void Main(string[] args)
         {
-            var db = new MusicHubDbContext();
-            //dbContext.Database.EnsureCreated();
-           // int producerId = int.Parse(Console.ReadLine());
+            var context = new MusicHubDbContext();
 
-            var albumsInfo = ExportAlbumsInfo(db, 9);
+            DbInitializer.ResetDatabase(context);
+            // int producerId = int.Parse(Console.ReadLine());
+
+            var albumsInfo = ExportAlbumsInfo(context, 9);
             Console.WriteLine(albumsInfo);
         }
 
 
+
         private static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            var albumsInfo = context.Albums
-                 .Where(p => p.ProducerId == producerId)
-                 .Select(a => new
-                 {
-                     AlbumName = a.Name,
-                     AlbumRealeaseDate = a.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
-                     ProducerName = a.Producer.Name,
-                     AlbumSongs = a.Songs.Select(s => new
-                     {
-                         SongName = s.Name,
-                         SongPrice = $"{s.Price:F2}",
-                         SongWriterName = s.Writer.Name
-                     })
+            var albumsInfo = context.
+                Producers
+                .FirstOrDefault(i => i.Id == producerId)
+                .Albums
+                .Select(a => new
+                {
+                    AlbumName = a.Name,
+                    AlbumRealeaseDate = a.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                    ProducerName = a.Producer.Name,
+                    AlbumSongs = a.Songs.Select(s => new
+                    {
+                        SongName = s.Name,
+                        SongPrice = $"{s.Price}",
+                        SongWriterName = s.Writer.Name
+                    })
                         .OrderByDescending(n => n.SongName)
                         .ThenBy(w => w.SongWriterName)
                         .ToList(),
-                     TotalAlbumPrice = $"{a.Price:F2}",
-                 })
+                    TotalAlbumPrice = $"{a.Price}",
+                })
                  .OrderByDescending(a => a.TotalAlbumPrice)
                  .ToList();
 
@@ -51,6 +59,7 @@ namespace MusicHub
                     .AppendLine($"-ProducerName: {album.ProducerName}");
 
                 int counter = 1;
+
                 foreach (var song in album.AlbumSongs)
                 {
                     sb
@@ -61,11 +70,16 @@ namespace MusicHub
                     counter++;
                 }
 
-                sb
-                    .AppendLine($"-AlbumPrice: {album.TotalAlbumPrice}");
+                sb.AppendLine($"-AlbumPrice: {album.TotalAlbumPrice}");
             }
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
