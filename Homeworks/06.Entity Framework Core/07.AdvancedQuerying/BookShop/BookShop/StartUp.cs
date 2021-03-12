5929611c716fd4b65dc31469c505b5df4521f7da
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-12 02:10:53 +0200

    Add some methods

--- Homeworks/06.Entity Framework Core/07.AdvancedQuerying/BookShop/BookShop/StartUp.cs
+++ Homeworks/06.Entity Framework Core/07.AdvancedQuerying/BookShop/BookShop/StartUp.cs
@@ -2,6 +2,9 @@
 {
     using Data;
     using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
 
     public class StartUp
     {
@@ -9,6 +12,85 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            ////01.AgeRestriction
+            //var input = Console.ReadLine();
+            //var result = GetBooksByAgeRestriction(db, input);
+            //Console.WriteLine(result);
+
+            ////02.Golden Books
+            //var result = GetGoldenBooks(db);
+            //Console.WriteLine(result);
+
+            //03.Books by Price
+            var result = GetBooksByPrice(db);
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var titles = context
+                .Books
+                .AsEnumerable()
+                .Where(t => t.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => b.Title)
+                .OrderBy(x => x)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var title in titles)
+            {
+                sb.AppendLine(title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context
+                .Books
+                .AsEnumerable()
+                .Where(c=>c.EditionType.ToString() == "Gold" && c.Copies < 5000)
+                .Select(b=> new 
+                {
+                    BookId = b.BookId,
+                    BookTitle = b.Title
+                })
+                .OrderBy(x=>x.BookId)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.BookTitle);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context
+                .Books
+                .Where(p => p.Price > 40)
+                .Select(b => new
+                {
+                    BookTitle = b.Title,
+                    BookPrice = b.Price
+                })
+                .OrderByDescending(x => x.BookPrice)
+                .ToList();
+                
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.BookTitle} - ${book.BookPrice:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
