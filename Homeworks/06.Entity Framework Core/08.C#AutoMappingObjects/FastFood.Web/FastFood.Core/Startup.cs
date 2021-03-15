e846766de9edecb9ec10816dcf978709bb239b02
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-16 01:28:54 +0200

    Make it to show numbers from 1 instead of 0

--- Homeworks/06.Entity Framework Core/08.C#AutoMappingObjects/FastFood.Web/FastFood.Core/Startup.cs
+++ Homeworks/06.Entity Framework Core/08.C#AutoMappingObjects/FastFood.Web/FastFood.Core/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace FastFood.Core
 {
+    using AutoMapper;
     using Data;
     using Microsoft.EntityFrameworkCore;
 
@@ -29,6 +30,8 @@ namespace FastFood.Core
             services.AddDbContext<FastFoodContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAutoMapper(typeof(Startup));
 
             services.AddControllersWithViews();
         }
