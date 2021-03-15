b86abdf6419529ce1c282e2b710ca7d9a07f4367
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-16 01:25:13 +0200

    Create Category Map

--- Homeworks/06.Entity Framework Core/08.C#AutoMappingObjects/FastFood.Web/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
+++ Homeworks/06.Entity Framework Core/08.C#AutoMappingObjects/FastFood.Web/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -1,6 +1,8 @@
 ﻿namespace FastFood.Core.MappingConfiguration
 {
     using AutoMapper;
+    using FastFood.Core.Controllers;
+    using FastFood.Core.ViewModels.Categories;
     using FastFood.Models;
     using ViewModels.Positions;
 
@@ -14,6 +16,11 @@
 
             this.CreateMap<Position, PositionsAllViewModel>()
                 .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(cm => cm.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>();
         }
     }
 }
