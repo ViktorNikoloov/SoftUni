144fff35b57576a87dc1bd162a9f13d8a6cabdd2
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-17 01:24:58 +0200

    Add ViewModels

--- Homeworks/06.Entity Framework Core/08.C#AutoMappingObjects/FastFood.Web/FastFood.Core/ViewModels/Items/CreateItemInputModel.cs
+++ Homeworks/06.Entity Framework Core/08.C#AutoMappingObjects/FastFood.Web/FastFood.Core/ViewModels/Items/CreateItemInputModel.cs
@@ -1,11 +1,18 @@
-﻿namespace FastFood.Core.ViewModels.Items
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastFood.Core.ViewModels.Items
 {
     public class CreateItemInputModel
     {
+        [StringLength(30, MinimumLength = 3)]
         public string Name { get; set; }
 
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
         public decimal Price { get; set; }
 
+        [Required]
         public int CategoryId { get; set; }
     }
 }
