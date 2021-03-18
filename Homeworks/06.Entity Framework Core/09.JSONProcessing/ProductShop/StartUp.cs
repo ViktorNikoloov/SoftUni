526d88f8990cb47135222c674f4b91f76c705cca
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-18 23:46:52 +0200

    Add new methods

--- Homeworks/06.Entity Framework Core/09.JSONProcessing/ProductShop/StartUp.cs
+++ Homeworks/06.Entity Framework Core/09.JSONProcessing/ProductShop/StartUp.cs
@@ -12,7 +12,52 @@ namespace ProductShop
     {
         public static void Main(string[] args)
         {
+            ProductShopContext db = new ProductShopContext();
 
+            //ResetDatabase(db);
+
+            ////02.Import Users
+            //string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            //string result = ImportUsers(db, inputJson);
+            //Console.WriteLine(result);
+
+            //03.Import Products
+            string inputJson = File.ReadAllText("../../../Datasets/products.json");
+            string result = ImportProducts(db, inputJson);
+            Console.WriteLine(result);
+        }
+
+        private static void ResetDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Database was successfully deleted!");
+            db.Database.EnsureCreated();
+            Console.WriteLine("Database was successfully created!");
+
+        }
+
+        //02.Import Users
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            User[] users = JsonConvert
+                .DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        //03.Import Products
+        static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            Product[] products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+
+            return $"Successfully imported {products.Length}";
         }
     }
 }
