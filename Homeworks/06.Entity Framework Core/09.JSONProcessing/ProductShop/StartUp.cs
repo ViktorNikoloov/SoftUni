4acf3f5a196a2a4a3f1d26fe94201b4543312a6e
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-21 01:28:43 +0200

    Finished ProductShop project

--- Homeworks/06.Entity Framework Core/09.JSONProcessing/ProductShop/StartUp.cs
+++ Homeworks/06.Entity Framework Core/09.JSONProcessing/ProductShop/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using ProductShop.Data;
 using ProductShop.Models;
@@ -14,46 +15,43 @@ namespace ProductShop
         {
             ProductShopContext db = new ProductShopContext();
 
-            //ResetDatabase(db);
+            ResetDatabase(db);
 
-            ////02.Import Users
-            //string inputJson = File.ReadAllText("../../../Datasets/users.json");
-            //string result = ImportUsers(db, inputJson);
-            //Console.WriteLine(result);
+            //02.Import Users
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            string result = ImportUsers(db, inputJson);
+            Console.WriteLine(result);
 
-            ////03.Import Products
-            //string inputJson = File.ReadAllText("../../../Datasets/products.json");
-            //string result = ImportProducts(db, inputJson);
-            //Console.WriteLine(result);
+            //03.Import Products
+            string inputJson = File.ReadAllText("../../../Datasets/products.json");
+            string result = ImportProducts(db, inputJson);
+            Console.WriteLine(result);
 
-            ////04.Import Categories
-            //string inputJson = File.ReadAllText("../../../Datasets/categories.json");
-            //string result = ImportCategories(db, inputJson);
-            //Console.WriteLine(result);
+            //04.Import Categories
+            string inputJson = File.ReadAllText("../../../Datasets/categories.json");
+            string result = ImportCategories(db, inputJson);
+            Console.WriteLine(result);
 
-            ////05.Import Categories and Products
-            //string inputJson = File.ReadAllText("../../../Datasets/categories-products.json");
-            //string result = ImportCategoryProducts(db, inputJson);
-            //Console.WriteLine(result);
+            //05.Import Categories and Products
+            string inputJson = File.ReadAllText("../../../Datasets/categories-products.json");
+            string result = ImportCategoryProducts(db, inputJson);
+            Console.WriteLine(result);
 
-            //////06.Export Products in Range
-            //string result = GetProductsInRange(db);
-            //Console.WriteLine(result);
+            //06.Export Products in Range
+            string result = GetProductsInRange(db);
+            Console.WriteLine(result);
 
-            ////07.Export Successfully Sold Products
-            //string result = GetSoldProducts(db);
-            //Console.WriteLine(result);
+            //07.Export Successfully Sold Products
+            string result = GetSoldProducts(db);
+            Console.WriteLine(result);
 
-            ////08.Export Categories by Products Count
-            //string result = GetCategoriesByProductsCount(db);
-            //Console.WriteLine(result);
+            //08.Export Categories by Products Count
+            string result = GetCategoriesByProductsCount(db);
+            Console.WriteLine(result);
 
             //08.Export Users and Products
             string result = GetUsersWithProducts(db);
             Console.WriteLine(result);
-
-
-
         }
 
         private static void ResetDatabase(ProductShopContext db)
@@ -204,15 +202,11 @@ namespace ProductShop
         //08.Export Users and Products
         public static string GetUsersWithProducts(ProductShopContext context)
         {
-            //et all users who have at least 1 sold product with a buyer.
-            //Order them in descending order by the number of sold products with a buyer.
-            //Select only their first and last name, age and for each product - name and price.
-            //Ignore all null values.
-
             var users = context
                 .Users
-                .Where(u => u.ProductsSold.Count() >= 1 && u.ProductsSold.Any(b => b.Buyer != null))
-                .OrderByDescending(x => x.ProductsSold.Count())
+                .Include(ps=>ps.ProductsSold)
+                .ToList()
+                .Where(u => u.ProductsSold.Count() >= 1 && u.ProductsSold.Any(b => b.BuyerId != null))
                 .Select(u => new
                 {
                     firstName = u.FirstName,
@@ -220,8 +214,8 @@ namespace ProductShop
                     age = u.Age,
                     soldProducts = new
                     {
-                        count = u.ProductsSold.Count(),
-                        products = u.ProductsSold.Select(sp => new
+                        count = u.ProductsSold.Where(b => b.BuyerId != null).Count(),
+                        products = u.ProductsSold.Where(b => b.BuyerId != null).Select(sp => new
                         {
                             name = sp.Name,
                             price = sp.Price
@@ -229,9 +223,16 @@ namespace ProductShop
                         .ToList()
                     }
                 })
+                .OrderByDescending(x => x.soldProducts.products.Count())
                 .ToList();
 
-            var json = JsonConvert.SerializeObject(users, new JsonSerializerSettings 
+            var resultObject = new
+            {
+                usersCount = users.Count(),
+                users = users,
+            };
+
+            var json = JsonConvert.SerializeObject(resultObject, new JsonSerializerSettings 
             {
                 Formatting = Formatting.Indented,
                 NullValueHandling = NullValueHandling.Ignore 
