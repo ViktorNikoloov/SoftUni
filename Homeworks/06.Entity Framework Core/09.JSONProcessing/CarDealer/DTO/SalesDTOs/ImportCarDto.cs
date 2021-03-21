7ac097fc42e19d70f2c17ec5b6c0520dcbff5438
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-21 03:04:27 +0200

    Add DTOs

---
+++ Homeworks/06.Entity Framework Core/09.JSONProcessing/CarDealer/DTO/SalesDTOs/ImportCarDto.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO.SalesDTOs
+{
+    public class ImportCarDto
+    {
+        [JsonProperty("make")]
+        public string Make { get; set; }
+
+        [JsonProperty("model")]
+        public string Model { get; set; }
+
+        [JsonProperty("travelledDistance")]
+        public long TravelledDistance { get; set; }
+
+        public int[] PartId { get; set; }
+    }
+}
