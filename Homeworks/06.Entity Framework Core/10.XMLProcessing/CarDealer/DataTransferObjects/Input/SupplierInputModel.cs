a734c138d53477b7e030aa5d8c800af64bd6e876
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-26 23:36:13 +0200

    Add 1.Import Suppliers method and SupplierInputModel

---
+++ Homeworks/06.Entity Framework Core/10.XMLProcessing/CarDealer/DataTransferObjects/Input/SupplierInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Input
+{
+    [XmlType("Supplier")]
+    public class SupplierInputModel
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
