78c6b0e908cf02cf1cdd2ff9c32149e675710685
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-27 04:43:32 +0200

    Add new methods and DTOs

---
+++ Homeworks/06.Entity Framework Core/10.XMLProcessing/CarDealer/DataTransferObjects/Output/CarDistanceModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Output
+{
+    [XmlType("car")]
+    public class CarDistanceModel
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("travelled-distance")]
+        public long TravelledDistance { get; set; }
+    }
+}
---
+++ Homeworks/06.Entity Framework Core/10.XMLProcessing/CarDealer/DataTransferObjects/Output/CustomerTotalSalesModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Output
+{
+    [XmlType("customer")]
+    public class CustomerTotalSalesModel
+    {
+        [XmlAttribute("full-name")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("bought-cars")]
+        public int BoughtCars { get; set; }
+
+        [XmlAttribute("spent-money")]
+        public decimal SpentMoney { get; set; }
+    }
+}
