0496b4a2bff8d715ca976d7ea2adca096ca46bfc
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-28 00:22:34 +0200

    Add 01.Import Users method and DTO

---
+++ Homeworks/06.Entity Framework Core/10.XMLProcessing/ProductShop/DataTransferObjects/Input/ImportUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DataTransferObjects.Input
+{
+    [XmlType("User")]
+    public class ImportUserModel
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public int? Age { get; set; }
+    }
+}
--- Homeworks/06.Entity Framework Core/10.XMLProcessing/ProductShop/StartUp.cs
+++ Homeworks/06.Entity Framework Core/10.XMLProcessing/ProductShop/StartUp.cs
@@ -1,13 +1,67 @@
-﻿namespace ProductShop
+﻿using ProductShop.Data;
+using ProductShop.DataTransferObjects.Input;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using AutoMapper;
+using ProductShop.Models;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            var db = new Data.ProductShopContext();
+            var db = new ProductShopContext();
+
+            InitializeMapper();
+
+            //01.Import Users
+            //var inputXml = File.ReadAllText("./Datasets/users.xml");
+            //var result = ImportUsers(db, inputXml);
+            //Console.WriteLine(result);
+
+            //02.Import Products
+            var inputXml = File.ReadAllText("./Datasets/products.xml");
+            var result = ImportUsers(db, inputXml);
+            Console.WriteLine(result);
+
 
         }
 
-        
+        private static void InitializeMapper()
+        {
+            Mapper.Initialize(cfg => { cfg.AddProfile<ProductShopProfile>(); });
+        }
+
+        //01.Import Users
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            const string root = "Users";
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportUserModel[]), new XmlRootAttribute(root));
+
+            var textReader = new StringReader(inputXml);
+
+            var usersDto = xmlSerializer
+                .Deserialize(textReader) as ImportUserModel[];
+            var mapUser = Mapper.Map<User[]>(usersDto);
+           
+            context.Users.AddRange(mapUser);
+            context.SaveChanges();
+
+            return $"Successfully imported {mapUser.Count()}";
+        }
+
+        //02.Import Products
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            const string root = "Products";
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(), XmlRootAttribute(root));
+
+        }
+
     }
 }
