4d409958580414576dd241c2c633c6d3df4bb9e7
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-27 04:44:08 +0200

    Add new methods and DTOs

---
+++ Homeworks/06.Entity Framework Core/10.XMLProcessing/CarDealer/DataTransferObjects/Output/CarLocalSuppliers.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Output
+{
+    [XmlType("suplier")]
+    public class CarLocalSuppliers
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("parts-count")]
+        public int PartsCount { get; set; }
+    }
+}
