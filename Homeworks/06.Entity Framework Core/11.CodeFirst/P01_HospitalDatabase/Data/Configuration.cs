2054a34cdbbd107eda49d0beb008da533f13afb5
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-29 00:46:48 +0300

    Add Data

---
+++ Homeworks/06.Entity Framework Core/11.CodeFirst/P01_HospitalDatabase/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    public static class Configuration
+    {
+        public static string ConfigurationString = @"Server=.\SQLEXPRESS;DataBase=HospitalDatabase;Integrated Security=true";
+    }
+}
---
+++ Homeworks/06.Entity Framework Core/11.CodeFirst/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data
+{
+
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+
+        }
+
+        public HospitalContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Visitation> Visitations { get; set; }
+        public DbSet<Diagnose> Diagnoses { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<PatientMedicament> Prescriptions { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConfigurationString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PatientMedicament>(x =>
+            {
+                x.HasKey(k => new
+                {
+                    k.MedicamentId,
+                    k.PatientId
+                });
+            });
+
+        }
+    }
+}
