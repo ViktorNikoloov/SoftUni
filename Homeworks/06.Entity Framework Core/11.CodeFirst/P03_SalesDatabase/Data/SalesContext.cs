428245ad9bc3bb2f4121accdb09d21039d9c7eeb
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-29 01:05:58 +0300

    Change the configuring path

--- Homeworks/06.Entity Framework Core/11.CodeFirst/P03_SalesDatabase/Data/SalesContext.cs
+++ Homeworks/06.Entity Framework Core/11.CodeFirst/P03_SalesDatabase/Data/SalesContext.cs
@@ -1,6 +1,33 @@
-﻿namespace P03_SalesDatabase.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P03_SalesDatabase.Data
 {
-    class SalesContext
+    public class SalesContext : DbContext
     {
+        public SalesContext()
+        {
+
+        }
+
+        public SalesContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConfigurationString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
