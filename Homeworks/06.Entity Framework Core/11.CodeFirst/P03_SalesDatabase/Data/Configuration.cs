dae4d6ab3b9417fa37487871abcf30411ebb089d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-29 01:02:32 +0300

    Add DbContext

---
+++ Homeworks/06.Entity Framework Core/11.CodeFirst/P03_SalesDatabase/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    internal static class Configuration
+    {
+        internal static string ConfigurationString = @"Server=.\SQLEXPRESS;DataBase=HospitalDatabase;Integrated Security=true";
+    }
+}
---
+++ Homeworks/06.Entity Framework Core/11.CodeFirst/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,6 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    class SalesContext
+    {
+    }
+}
