e52b773fc1559ed520ac70b6e0d628b7a989909c
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-28 05:34:06 +0300

    07.Categories By Products Count method

---
+++ Homeworks/06.Entity Framework Core/10.XMLProcessing/ProductShop/DataTransferObjects/Output/CategoriesProductsCountModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DataTransferObjects.Output
+{
+    [XmlType("Category")]
+    public class CategoriesProductsCountModel
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlElement("averagePrice")]
+        public decimal AveragePrice { get; set; }
+
+        [XmlElement("totalRevenue")]
+        public decimal TotalRevenue { get; set; }
+
+    }
+}
