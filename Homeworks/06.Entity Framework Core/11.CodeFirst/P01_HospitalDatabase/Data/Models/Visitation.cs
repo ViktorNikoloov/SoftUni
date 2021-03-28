d2e8a55e4e564236c4e69171f16bbc1632da0ede
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-29 01:01:16 +0300

    ApplyDoctor functionality

--- Homeworks/06.Entity Framework Core/11.CodeFirst/P01_HospitalDatabase/Data/Models/Visitation.cs
+++ Homeworks/06.Entity Framework Core/11.CodeFirst/P01_HospitalDatabase/Data/Models/Visitation.cs
@@ -16,5 +16,8 @@ namespace P01_HospitalDatabase.Data.Models
 
         public int PatientId { get; set; }
         public Patient Patient { get; set; }
+
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
     }
 }
--- Homeworks/06.Entity Framework Core/11.CodeFirst/P01_HospitalDatabase/Migrations/20210328215540_AddDoctorFunctionality.Designer.cs
+++ Homeworks/06.Entity Framework Core/11.CodeFirst/P01_HospitalDatabase/Migrations/20210328215540_AddDoctorFunctionality.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using P01_HospitalDatabase.Data;
 
 namespace P01_HospitalDatabase.Migrations
 {
     [DbContext(typeof(HospitalContext))]
-    partial class HospitalContextModelSnapshot : ModelSnapshot
+    [Migration("20210328215540_AddDoctorFunctionality")]
+    partial class AddDoctorFunctionality
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
