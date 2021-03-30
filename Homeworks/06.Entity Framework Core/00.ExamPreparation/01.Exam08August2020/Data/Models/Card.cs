a91047e2d161458648d13f8d741f966394d1bf61
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-03-31 02:09:52 +0300

    Change regex attribute

--- Homeworks/06.Entity Framework Core/00.ExamPreparation/01.Exam08August2020/Data/Models/Card.cs
+++ Homeworks/06.Entity Framework Core/00.ExamPreparation/01.Exam08August2020/Data/Models/Card.cs
@@ -15,11 +15,11 @@ namespace VaporStore.Data.Models
         public int Id { get; set; }
 
         [Required]
-        [RegularExpression(@"^[1-9]{4}\s+[1-9]{4}\s +[1 - 9]{ 4}\s +[1 - 9]{ 4}")]
+        [RegularExpression(@"([0-9]{4} *){4}")]
         public string Number { get; set; } 
 
         [Required]
-        [RegularExpression(@"^[1-9]{3}")]
+        [RegularExpression(@"^[0-9]{3}")]
         public string Cvc { get; set; }
 
         [Required]
