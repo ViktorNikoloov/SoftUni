51ee03f9615830d1dcce474ef4ad01733dadac0b
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-08 00:32:29 +0300

    Add DbContext

---
+++ Homeworks/06.Entity Framework Core/12.BestPracticesAndArchitecture/RealEstates/RealEstates.Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Models;
+
+namespace RealEstates.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConfigurationString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PropertyTag>()
+                .HasKey(pt => new
+                {
+                    pt.PropertyId,
+                    pt.TagId
+                });
+        }
+    }
+}
