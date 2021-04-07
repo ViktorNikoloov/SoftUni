07d9e36285c9ca52288a08618d91d13e86615782
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-08 00:34:18 +0300

    Add RealEstates.Models Library

---
+++ Homeworks/06.Entity Framework Core/12.BestPracticesAndArchitecture/RealEstates/RealEstates.Models/Property.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace RealEstates.Models
+{
+    public class Property
+    {
+        public Property()
+        {
+            Tags = new HashSet<Tag>();
+        }
+
+        public int Id { get; set; }
+
+        public int Size { get; set; }
+
+        public int? YardSize { get; set; }
+
+        public byte? Floor { get; set; }
+
+        public byte? TotalFloor { get; set; }
+
+        public int? Year { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property price in Euro
+        /// </summary>
+        public int Price { get; set; }
+
+        public int DistrictId { get; set; }
+        public virtual District District { get; set; }
+
+        public int TypeId { get; set; }
+        public virtual PropertyType Type { get; set; }
+
+        public int BuildingTypeId { get; set; }
+        public virtual PropertyType BuildingType { get; set; }
+
+        public int TagId { get; set; }
+        public virtual Tag Tag { get; set; }
+
+        public virtual ICollection<Tag> Tags { get; set; }
+
+    }
+}
---
+++ Homeworks/06.Entity Framework Core/12.BestPracticesAndArchitecture/RealEstates/RealEstates.Models/PropertyTag.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstates.Models
+{
+    public class PropertyTag
+    {
+        public int PropertyId { get; set; }
+        public virtual Property Property { get; set; }
+
+        public int TagId { get; set; }
+        public virtual Tag Tag { get; set; }
+    }
+}
