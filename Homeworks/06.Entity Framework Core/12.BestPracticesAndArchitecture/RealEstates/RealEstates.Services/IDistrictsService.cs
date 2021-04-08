cc10022c53e7cf689b3483f2c86b5cf9f4c0fe12
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-09 00:01:18 +0300

    Add Services and DTOs

---
+++ Homeworks/06.Entity Framework Core/12.BestPracticesAndArchitecture/RealEstates/RealEstates.Services/IDistrictsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using RealEstates.Services.Models;
+
+namespace RealEstates.Services
+{
+    public interface IDistrictsService
+    {
+        IEnumerable<DistrictInfoDto> GetMostExpensiveDistricts(int count);
+    }
+}
---
+++ Homeworks/06.Entity Framework Core/12.BestPracticesAndArchitecture/RealEstates/RealEstates.Services/IPropertiesService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using RealEstates.Services.Models;
+
+namespace RealEstates.Services
+{
+    public interface IPropertiesService
+    {
+        void Add(string district, int floor, int maxFloor, int size, int yardSize, int year, string properyType, string buildingType, int price);
+
+        IEnumerable<PropertyInfoDto> Search(int minPrice, int maxPrice, int minSize, int maxSize);
+    }
+}
