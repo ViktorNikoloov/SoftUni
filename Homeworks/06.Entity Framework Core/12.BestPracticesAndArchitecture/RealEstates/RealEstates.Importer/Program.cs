a616173d400166e9d1e3916087321993224197ba
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-15 23:53:24 +0300

    Apply ImportJsonFile method into RealEstates.Importer

---
+++ Homeworks/06.Entity Framework Core/12.BestPracticesAndArchitecture/RealEstates/RealEstates.Importer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RealEstates.Data;
+using RealEstates.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace RealEstates.Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportJsonFile("imot.bg-houses-Sofia-raw-data-2021-03-18.json");
+            Console.WriteLine();
+            ImportJsonFile("imot.bg-raw-data-2021-03-18.json");
+        }
+
+        public static void ImportJsonFile(string fileName)
+        {
+            var dbContext = new ApplicationDbContext();
+            IPropertiesService propertiesService = new PropertiesService(dbContext);
+
+            var properties = JsonSerializer
+                .Deserialize<IEnumerable<PropertyAsJson>>(File.ReadAllText(fileName));
+            foreach (var jsonProp in properties)
+            {
+                propertiesService.Add(jsonProp.District, jsonProp.Price, jsonProp.Floor, jsonProp.TotalFloor,
+                    jsonProp.Size, jsonProp.YardSize, jsonProp.Year, jsonProp.Type, jsonProp.BuildingType);
+                Console.Write("."); //Just to see importer speed
+            }
+        }
+    }
+}
