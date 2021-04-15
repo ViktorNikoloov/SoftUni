319e134ecea9566fdaa9e5a07e0c42f46ffeb8a4
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-16 00:30:43 +0300

    Apply GetMostExpensiveDistricts method in DistrictService

---
+++ Homeworks/06.Entity Framework Core/12.BestPracticesAndArchitecture/RealEstates/RealEstates.Services/DistrictService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RealEstates.Data;
+using RealEstates.Services.Models;
+
+namespace RealEstates.Services
+{
+    public class DistrictService : IDistrictsService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public DistrictService(ApplicationDbContext dbContext )
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IEnumerable<DistrictInfoDto> GetMostExpensiveDistricts(int count)
+        {
+            var districts = dbContext
+                .Districts
+                .Select(x => new DistrictInfoDto()
+                {
+                    Name = x.Name,
+                    PropertiesCount = x.Properties.Count,
+                    AveragePricePerSquareMeter = x.Properties
+                       .Where(x => x.Price.HasValue)
+                       .Average(p => p.Price / (decimal)p.Size) ?? 0
+                })
+                .OrderByDescending(x=>x.AveragePricePerSquareMeter)
+                .Take(count)
+                .ToList();
+
+            return districts;
+        }
+    }
+}
