17fb840bb47f9b621f6e349a437c0eaa2f507624
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-22 01:10:15 +0300

    Add AnswerService

---
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Services/AnswerService.cs
@@ -0,0 +1,31 @@
+﻿using Quiz.Data;
+using Quiz.Models;
+
+namespace Quiz.Services
+{
+    public class AnswerService : IAnswerService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public AnswerService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public int Add(string title, int points, bool isCorrenct, int questionId)
+        {
+            var answer = new Answer
+            {
+                Title = title,
+                Points = points,
+                IsCorrect = isCorrenct,
+                QuestionId = questionId
+            };
+
+            applicationDbContext.Answers.Add(answer);
+            applicationDbContext.SaveChanges();
+
+            return answer.Id;
+        }
+    }
+}
---
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Services/IAnswerService.cs
@@ -0,0 +1,7 @@
+﻿namespace Quiz.Services
+{
+    public interface IAnswerService
+    {
+        int Add(string title, int points, bool isCorrenct, int questionId);
+    }
+}
