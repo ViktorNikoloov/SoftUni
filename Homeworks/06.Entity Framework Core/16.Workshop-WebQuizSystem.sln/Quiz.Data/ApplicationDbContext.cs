841f0c7cf7ce13f2ba9ec5a7e0c2b7a081102486
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-21 15:36:01 +0300

    Apply DesingTimeDbContextFactory

--- Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Data/ApplicationDbContext.cs
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Data/ApplicationDbContext.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
+
+using Quiz.Models;
 
 namespace Quiz.Data
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
+        public ApplicationDbContext()
+        {
+                
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        public DbSet<Models.Quiz> Quizzes { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Answer> Answers { get; set; }
+
     }
+
 }
