ca25e04d0a706949be41da4df20a49a0da07d9fa
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-22 01:31:32 +0300

    Add ViewModels

---
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Services/Models/QuizViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz.Services.Models
+{
+   public class QuizViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public IEnumerable<QuestionViewModel> Questions { get; set; }
+    }
+}
