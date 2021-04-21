39c84f0f4ffd9ec8c98781e61c55d2479d1623d7
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-21 23:47:46 +0300

    Config Quiz.ConsoleUI

--- Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.ConsoleUI/Program.cs
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.ConsoleUI/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Quiz.Data;
+using System.IO;
 
 namespace Quiz.ConsoleUI
 {
@@ -6,7 +11,31 @@ namespace Quiz.ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var dbContex = serviceProvider.GetService<ApplicationDbContext>();
+
+            foreach (var item in dbContex.Users)
+            {
+                System.Console.WriteLine(item.UserName);
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            services.AddDbContext<ApplicationDbContext>(options
+                => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options
+                => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
         }
     }
 }
