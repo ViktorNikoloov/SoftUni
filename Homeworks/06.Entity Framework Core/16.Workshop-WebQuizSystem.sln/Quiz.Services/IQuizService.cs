5b1f8a9ede8c9a0c79facd18ca221d348753bf81
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-22 01:20:43 +0300

    Add GetQuizById method

--- Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Services/IQuizService.cs
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Services/IQuizService.cs
@@ -1,7 +1,11 @@
-﻿namespace Quiz.Services
+﻿using Quiz.Services.Models;
+
+namespace Quiz.Services
 {
     public interface IQuizService
     {
         void Add(string title);
+
+        QuizViewModel GetQuizById(int quizId);
     }
 }
