4784f7217c1da48a80d71f459c65c364103de2c7
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-21 15:49:31 +0300

    Add UserAnswer model

---
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Models/UserAnswer.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz.Models
+{
+    public class UserAnswer
+    {
+        public int UserId { get; set; }
+
+        public int QuizId { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public int AnswerId { get; set; }
+    }
+}
