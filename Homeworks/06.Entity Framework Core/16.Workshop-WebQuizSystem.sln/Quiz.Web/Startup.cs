92ec403ddbc51f28a1288aa9a86d7077d0e425d0
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-21 15:44:08 +0300

    Chnage some usings

--- Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Web/Startup.cs
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Quiz.Web.Data;
+using Quiz.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
