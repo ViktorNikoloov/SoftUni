bf82bc12faff08f0dbaf0a4e3813a9866e0ea258
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-04-26 00:49:50 +0300

    Apply QuizController

---
+++ Homeworks/06.Entity Framework Core/16.Workshop-WebQuizSystem.sln/Quiz.Web/Controllers/QuizController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using Quiz.Services;
+
+namespace Quiz.Web.Controllers
+{
+    [Authorize]
+    public class QuizController : Controller
+    {
+        private readonly IQuizService quizService;
+        private readonly IUserAnswerService userAnswerService;
+
+        public QuizController(IQuizService quizService,
+            IUserAnswerService userAnswerService)
+        {
+            this.quizService = quizService;
+            this.userAnswerService = userAnswerService;
+        }
+
+        public IActionResult Test(int id)
+        {
+            quizService.StartQuiz(User?.Identity?.Name, id);
+            var viewModel = quizService.GetQuizById(id);
+            return View(viewModel);
+
+        }
+
+        public IActionResult Submit(int id)
+        {
+            foreach (var item in Request.Form)
+            {
+                var questionId = int.Parse(item.Key.Replace("q_", string.Empty));
+                var answerId = int.Parse(item.Value);
+                userAnswerService.AddUserAnswer(User.Identity.Name, questionId, answerId);
+            }
+
+            return RedirectToAction("Results", new {id});
+            
+        }
+        public IActionResult Results(int id)
+        {
+            var points = userAnswerService.GetUserResult(User.Identity.Name, id);
+            return View(points);
+        }
+    }
+}
