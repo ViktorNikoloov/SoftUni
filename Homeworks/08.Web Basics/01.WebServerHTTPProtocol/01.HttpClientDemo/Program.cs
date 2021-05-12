343ca00044888f862ef698a578ba7627972257a0
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-12 04:02:55 +0300

    Add 08.Web Basics course's  01.WebServerHTTPProtocol solution and 01.HttpClientDemo project

---
+++ Homeworks/08.Web Basics/01.WebServerHTTPProtocol/01.HttpClientDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.HttpClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReadData());
+        }
+
+        public static async Task ReadData()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string url = "https://softuni.bg/trainings/3353/csharp-web-basics-basics-may-2021/internal";
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("X-Test", "test...");
+            var html = await httpClient.GetStringAsync(url);
+            Console.WriteLine(html);
+        }
+    }
+
+}
