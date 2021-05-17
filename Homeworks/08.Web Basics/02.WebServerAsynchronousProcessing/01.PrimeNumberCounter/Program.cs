8fcd9e48ac81cff00c9214d7a6cd8d069c27c222
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-18 00:24:03 +0300

    Add 01.PrimeNumberCounter project

---
+++ Homeworks/08.Web Basics/02.WebServerAsynchronousProcessing/01.PrimeNumberCounter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _01.PrimeNumberCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Count and Time at the begining
+            //664580
+            //00:00:13.8449974
+            Stopwatch sw = Stopwatch.StartNew();
+
+            int n = 10_000_000;
+            int count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+            Console.WriteLine(sw.Elapsed);
+        }
+    }
+}
