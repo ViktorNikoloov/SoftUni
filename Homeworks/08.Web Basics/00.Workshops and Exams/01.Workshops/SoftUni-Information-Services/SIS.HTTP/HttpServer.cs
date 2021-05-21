eebc418ad2bc95c5357742d52f36182a4b084f5d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-22 01:44:54 +0300

    Apply HttpServer's functionality

--- Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/HttpServer.cs
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/HttpServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,8 +10,7 @@ namespace SIS.HTTP
 {
     public class HttpServer : IHttpServer
     {
-        private const int BufferSize = 4096;
-        private const string NewLine = "\r\n";
+        
 
         IDictionary<string, Func<HttpRequest, HttpResponse>> routeTable =
             new Dictionary<string, Func<HttpRequest, HttpResponse>>();
@@ -48,7 +48,7 @@ namespace SIS.HTTP
             List<byte> data = new List<byte>();
 
             int position = 0;
-            byte[] buffer = new byte[BufferSize];
+            byte[] buffer = new byte[HttpConstants.BufferSize];
             while (true)
             {
                 int count =
@@ -70,22 +70,20 @@ namespace SIS.HTTP
 
             //byte[] => string => Encoding
             var requestAsString = Encoding.UTF8.GetString(data.ToArray());
-            Console.WriteLine(requestAsString);
+            var request = new HttpRequest(requestAsString);
+            Console.WriteLine($"{request.Method} {request.Path} => {request.Headers.Count} headers");
 
-            // TODO: extract info requestAsString
-
-            var responseHtml = "<h1>Welcome!</h1>";
+            var responseHtml = "<h1>Welcome!</h1>" +
+                request.Headers.FirstOrDefault(x=>x.Name =="User-Agent")?.Value;
             var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-
-            var responseHttp = "HTTP/1.1 200 OK" + NewLine +
-                "Server: SIS Server 1.0" + NewLine +
-                "Content-Type: text/html" + NewLine +
-                "Content-Length" + responseBodyBytes.Length + NewLine +
-                NewLine;
-            var responseHeaderBytes = Encoding.UTF8.GetBytes(responseHttp);
-
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            response.Headers.Add(new Header("Server", "SIS Server 1.0"));
+            response.Cookies.Add(new ResponseCookie("sid", Guid.NewGuid().ToString()) 
+            { HttpOnly = true, MaxAge = 60 * 24 * 60 * 60 });
+            
+            var responseHeaderBytes = Encoding.UTF8.GetBytes(response.ToString());
             await stream.WriteAsync(responseHeaderBytes, 0, responseHeaderBytes.Length);
-            await stream.WriteAsync(responseBodyBytes, 0, responseBodyBytes.Length);
+            await stream.WriteAsync(response.Body, 0, response.Body.Length);
 
             //tcpClient.Close();
         }
