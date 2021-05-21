50d70740529ca8c9f8f18f39c97b58e1141370a8
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-22 01:43:52 +0300

    Add ResponseCookie

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/ResponseCookie.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace SIS.HTTP
+{
+    public class ResponseCookie : Cookie
+    {
+        public ResponseCookie(string name, string value) 
+            : base(name, value)
+        {
+            Path = "/";
+        }
+
+        public int MaxAge { get; set; }
+
+        public bool HttpOnly { get; set; }
+
+        public string Path { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder cookieBuilder = new StringBuilder();
+            cookieBuilder.Append($"{Name}={Value}; Path={Path};");
+
+            if (MaxAge != 0)
+            {
+                cookieBuilder.Append($" Max-Age={MaxAge};");
+            }
+
+            if(HttpOnly)
+            {
+                cookieBuilder.Append(" HttpOnly;");
+            }
+
+            return cookieBuilder.ToString();
+        }
+
+        //Domain
+
+        //Secure
+    }
+}
