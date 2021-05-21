cd382597d2b600845a27aa3c6a6066ed89442acf
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-22 00:22:27 +0300

    Add Cookie and Header

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/Header.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SIS.HTTP
+{
+    public class Header
+    {
+        public Header(string headerLine)
+        {
+            var headerParts = headerLine
+                .Split(": ", 2, StringSplitOptions.None);
+
+            Name = headerParts[0];
+            Value = headerParts[1];
+        }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
