258ffc53387acd2064140ca6818bf373b0efbc02
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-22 00:42:08 +0300

    Override  ToString method

--- Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/Header.cs
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/Header.cs
@@ -16,5 +16,10 @@ namespace SIS.HTTP
         public string Name { get; set; }
 
         public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return "{Name}: {Value}";
+        }
     }
 }
