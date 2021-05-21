0a85c9e955091cecced59e1b7cf1ffc5baca6f2c
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-22 00:49:18 +0300

    Apply Cookies's functionality

--- Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/Cookie.cs
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/Cookie.cs
@@ -2,8 +2,20 @@
 {
     public class Cookie
     {
+        public Cookie(string cookieAsString)
+        {
+            var cookieParts = cookieAsString.Split('=', 2);
+
+            Name = cookieParts[0];
+            Value = cookieParts[1];
+        }
         public string Name { get; set; }
 
         public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}={Value}";
+        }
     }
 }
