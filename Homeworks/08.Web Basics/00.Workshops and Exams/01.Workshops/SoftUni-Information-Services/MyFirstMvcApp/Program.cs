0eafab19102ccda3bcb26413f7a05bfa68cb95cf
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-21 13:20:46 +0300

    Add MyFirstMvsApp project

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
