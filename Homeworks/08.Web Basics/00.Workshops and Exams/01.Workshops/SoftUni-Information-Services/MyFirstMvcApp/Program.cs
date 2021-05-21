b1716531b61ef663cedbf250154710ca84bc0293
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-21 15:03:47 +0300

    Add new functionalities

--- Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Program.cs
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using SIS.HTTP;
+using System;
+using System.Threading.Tasks;
 
 namespace MyFirstMvcApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IHttpServer server = new HttpServer();
+            
+
+            /*route table*/
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/favico.ico", Favicon);
+            server.AddRoute("/about", About);
+            server.AddRoute("/users/login", Login);
+
+            await server.StartAsync(80);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse About(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse Login(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static HttpResponse Favicon(HttpRequest arg)
+        {
+            throw new NotImplementedException();
         }
     }
 }
