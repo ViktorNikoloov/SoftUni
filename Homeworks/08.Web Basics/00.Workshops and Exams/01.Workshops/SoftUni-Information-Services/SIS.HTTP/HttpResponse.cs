2a916aa7650a072a7be2f64a220eef5345002fab
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-22 01:44:35 +0300

    Apply HttpResponse's functionality

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.HTTP/HttpResponse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SIS.HTTP.Enums;
+
+namespace SIS.HTTP
+{
+    public class HttpResponse
+    {
+        public HttpResponse(string contentType, byte[] body, HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(nameof(body));
+                //body = new byte[0];
+            }
+
+            StatusCode = statusCode;
+            Body = body;
+            Headers = new List<Header>
+            {
+                { new Header("Content-Type", contentType) },
+                { new Header("Content-Length", body.Length.ToString()) },
+            };
+
+            Cookies = new List<Cookie>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder responseBuilder = new StringBuilder();
+            responseBuilder.Append($"HTTP/1.1 {(int)StatusCode} {StatusCode}" + HttpConstants.NewLine);
+            foreach (var header in Headers)
+            {
+                responseBuilder.Append(header.ToString() + HttpConstants.NewLine);
+            }
+
+            foreach (var cookie in Cookies)
+            {
+                responseBuilder.Append($"Set-Cookie: {cookie.ToString()}{HttpConstants.NewLine}");
+            }
+            responseBuilder.Append(HttpConstants.NewLine);
+
+            return responseBuilder.ToString();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public ICollection<Header> Headers { get; set; }
+
+        public ICollection<Cookie> Cookies { get; set; }
+
+        public byte[] Body { get; set; }
+    }
+}
