16157ee090ee4ab73dce19ef5ae04f1e4d28a3ce
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-27 02:17:46 +0300

    Apply Controler abstract class

--- Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Program.cs
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Program.cs
@@ -13,8 +13,11 @@ namespace MyFirstMvcApp
 
             /*route table*/
             server.AddRoute("/", new HomeController().Index);
-            server.AddRoute("/about", new HomeController().About);
             server.AddRoute("/users/login", new UsersController().Login);
+            server.AddRoute("/users/register", new UsersController().Register);
+            server.AddRoute("/cards/add", new CardsController().Add);
+            server.AddRoute("/cards/all", new CardsController().All);
+            server.AddRoute("/cards/collection", new CardsController().Collection);
             server.AddRoute("/favicon.ico", new StaticFilesController().Favicon);
 
             //If we want to auto-start the HomePage
--- Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.MvcFramework/Controller.cs
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.MvcFramework/Controller.cs
@@ -1,6 +1,19 @@
-﻿namespace SIS.MvcFramework
+﻿using System.IO;
+using System.Text;
+
+using SIS.HTTP;
+
+namespace SIS.MvcFramework
 {
     public abstract class Controller
     {
+        public HttpResponse View(string viewPath)
+        {
+            var responseHtml = File.ReadAllText(viewPath);
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+
+            return response;
+        }
     }
 }
