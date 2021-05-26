b361fe2a8478d9ddce5584115f0176eb7e9e01a0
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-27 02:14:46 +0300

    Add CardsController

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Controllers/CardsController.cs
@@ -0,0 +1,23 @@
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class CardsController : Controller
+    {
+        public HttpResponse Add(HttpRequest request)
+        {
+            return View("Views/Cards/Add.cshtml");
+        }
+
+        public HttpResponse All(HttpRequest request)
+        {
+            return View("Views/Cards/All.cshtml");
+        }
+
+        public HttpResponse Collection(HttpRequest request)
+        {
+            return View("Views/Cards/Collection.cshtml");
+        }
+    }
+}
