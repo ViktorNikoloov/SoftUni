48805f3659f50d26ecd33265cbf75f2cdcc7ca1d
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-28 23:59:17 +0300

    Apply Test Driven Development Tests

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/Sis.MvcFramework.Tests/SisViewEngineTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace Sis.MvcFramework.Tests
+{
+    public class SisViewEngineTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var viewModel = new TestViewModel
+            {
+                DateOfBirth = new DateTime(2021, 05, 10),
+                Name = "Doggo Arghentino",
+                Price = 12345.67M,
+
+            };
+
+            string view = @"";
+        }
+    }
+
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
