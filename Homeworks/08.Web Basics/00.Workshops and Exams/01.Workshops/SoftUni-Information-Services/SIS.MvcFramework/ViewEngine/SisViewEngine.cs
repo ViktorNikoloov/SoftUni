464cbe0467c6bb501056383d6f8834d1da2a2ccb
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-28 23:19:13 +0300

    Apply SisViewEngine - not implemented yet

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.MvcFramework/ViewEngine/SisViewEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace SIS.MvcFramework.ViewEngine
+{
+    public class SisViewEngine : IViewEngine
+    {
+        public string GetHtml(string templateCode, object viewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
