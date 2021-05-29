c8e5f64d0492a3f35d53e6c06c12fadd2e305198
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-05-29 03:28:53 +0300

    Apply ErrorView

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/SIS.MvcFramework/ViewEngine/ErrorView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIS.MvcFramework.ViewEngine
+{
+    public class ErrorView : IView
+    {
+        private readonly IEnumerable<string> errors;
+        private readonly string csharpCode;
+
+        public ErrorView(IEnumerable<string> errors, string csharpCode)
+        {
+            this.errors = errors;
+            this.csharpCode = csharpCode;
+        }
+
+        public string CsharpCode { get; }
+
+        public string ExecuteTemplate(object viewModel)
+        {
+            var html = new StringBuilder();
+            html.AppendLine($"<h1>View compile {this.errors.Count()} errors:</h1><ul>");
+
+            foreach (var error in errors)
+            {
+                html.AppendLine($"<li>{error}</li>");
+            }
+
+            html.AppendLine($"</ul<pre>{csharpCode}</pre>");
+
+            return html.ToString();
+        }
+    }
+}
