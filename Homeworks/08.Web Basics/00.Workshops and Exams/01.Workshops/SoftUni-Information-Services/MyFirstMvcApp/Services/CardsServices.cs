07bf1ef74b595c409daaa4f323364e4652ae3fdd
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-06-09 00:54:12 +0300

    Fix the case where the response's Body is null

--- Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Services/CardsServices.cs
+++ Homeworks/08.Web Basics/00.Workshops and Exams/01.Workshops/SoftUni-Information-Services/MyFirstMvcApp/Services/CardsServices.cs
@@ -1,7 +1,16 @@
-﻿namespace MyFirstMvcApp.Services
+﻿using MyFirstMvcApp.Data;
+
+namespace MyFirstMvcApp.Services
 {
     public class CardsServices : ICardsService
     {
+        private readonly ApplicationDbContext db;
+
+        public CardsServices(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
         public void AddCard()
         {
             throw new System.NotImplementedException();
