8e20fde96ab0527c230cdcdd246148e343fb8af6
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-06-25 01:44:40 +0300

    DataConstants and UsersService

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/00.Exams/04.Exam23Dec2020/CarShop/Services/Users/UsersService.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+
+using CarShop.Data;
+using CarShop.Models;
+using CarShop.Services.PasswordHasher;
+
+using CarShop.ViewModels;
+using CarShop.ViewModels.Users;
+
+using static CarShop.Data.DataConstants;
+
+namespace CarShop.Services.Users
+{
+    public class UsersService : IUsersService
+    {
+        private readonly CarShopDbContext db;
+        
+        private readonly IPasswordHasher passwordHasher;
+
+        public UsersService(
+            CarShopDbContext db,
+            
+            IPasswordHasher passwordHasher)
+        {
+            this.db = db;
+            
+            this.passwordHasher = passwordHasher;
+        }
+
+        public void CreateUser(RegisterUserFormModel model)
+        {
+
+            var user = new User
+            {
+                Username = model.Username,
+                Password = passwordHasher.HashPassword(model.Password),
+                Email = model.Email,
+                IsMechanic = model.UserType == UserTypeMechanic
+            };
+
+            db.Users.Add(user);
+
+            db.SaveChanges();
+        }
+
+        public string IsUserExist(LoginUserFormModel model)
+        {
+            var hashedPassword = passwordHasher.HashPassword(model.Password);
+
+            var userId = db
+                .Users
+                .Where(u => u.Username == model.Username && u.Password == hashedPassword)
+                .Select(u => u.Id)
+                .FirstOrDefault();
+
+            return userId;
+        }
+    }
+}
