fef2c04247e5d296c37553d98df34b28f9ad9039
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-06-25 01:42:52 +0300

    Add PasswordHasher and  DataValidator

---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/00.Exams/04.Exam23Dec2020/CarShop/Services/PasswordHasher/PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace CarShop.Services.PasswordHasher
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            var bytes = Encoding.UTF8.GetBytes(password);
+            using var hash = SHA512.Create();
+            var hashedInputBytes = hash.ComputeHash(bytes);
+
+            // Convert to text
+            // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+            var hashedInputStringBuilder = new StringBuilder(128);
+            foreach (var b in hashedInputBytes)
+                hashedInputStringBuilder.Append(b.ToString("X2"));
+
+            return hashedInputStringBuilder.ToString();
+        }
+    }
+}
---
+++ Homeworks/08.Web Basics/00.Workshops and Exams/00.Exams/04.Exam23Dec2020/CarShop/Services/Validator/Validator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using CarShop.Data;
+using CarShop.ViewModels;
+
+using static CarShop.Data.DataConstants;
+
+namespace CarShop.Services.Validator
+{
+    public class Validator : IValidator
+    {
+        private readonly CarShopDbContext db;
+
+        public Validator(CarShopDbContext db)
+        {
+            this.db = db;
+        }
+
+        public ICollection<string> ValidateUser(RegisterUserFormModel user)
+        {
+            var errors = new List<string>();
+
+            if (user.Username == null || user.Username.Length < UserMinUsername || user.Username.Length > DefaultMaxLength)
+            {
+                //errors.Add(string.Format(ErrorViewText, $"Username '{user.Username}' is not valid. It must be between {UserMinUsername} and {DefaultMaxLength} characters long."));
+
+                errors.Add($"Username '{user.Username}' is not valid. It must be between {UserMinUsername} and {DefaultMaxLength} characters long.");
+            }
+
+            if (user.Email == null || !Regex.IsMatch(user.Email, UserEmailRegularExpression))
+            {
+                errors.Add($"Email '{user.Email}' is not a valid e-mail address.");
+            }
+
+            if (user.Password == null || user.Password.Length < UserMinPassword || user.Password.Length > DefaultMaxLength)
+            {
+                errors.Add($"The provided password is not valid. It must be between {UserMinPassword} and {DefaultMaxLength} characters long.");
+            }
+
+            if (user.Password != null && user.Password.Any(x => x == ' '))
+            {
+                errors.Add($"The provided password cannot contain whitespaces.");
+            }
+
+            if (user.Password != user.ConfirmPassword)
+            {
+                errors.Add("Password and its confirmation are different.");
+            }
+
+            if (user.UserType != UserTypeClient && user.UserType != UserTypeMechanic)
+            {
+                errors.Add($"The user can be either a '{UserTypeClient}' or a '{UserTypeMechanic}'.");
+            }
+
+            if (this.db.Users.Any(u => u.Username == user.Username))
+            {
+                errors.Add($"User with '{user.Username}' username already exists.");
+            }
+
+            if (this.db.Users.Any(u => u.Email == user.Email))
+            {
+                errors.Add($"User with '{user.Email}' e-mail already exists.");
+            }
+            return errors;
+        }
+    }
+}
