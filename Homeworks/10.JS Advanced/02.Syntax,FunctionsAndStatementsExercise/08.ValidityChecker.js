3101f3299b65484c406b8c6025891af2c158dd87
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-09-20 00:23:43 +0300

    refactoring

--- Homeworks/10.JS Advanced/02.Syntax,FunctionsAndStatementsExercise/08.ValidityChecker.js
+++ Homeworks/10.JS Advanced/02.Syntax,FunctionsAndStatementsExercise/08.ValidityChecker.js
@@ -1,23 +1,15 @@
-function validate(points) {
-    let x1 = parseInt(points[0]);
-    let y1 = parseInt(points[1]);
-    let x2 = parseInt(points[2]);
-    let y2 = parseInt(points[3]);
+function validate(x1, y1, x2, y2) {
 
-    let func = (x1,y1,x2,y2)=>{
-        let height =x1 - x2;
-        let width =  y1 - y2;
-        let distance = Math.hypot(height,width);
-        if (Number.isInteger(distance)) {
-            return true;
-        }
-        return false;
+    function getResult(x1, y1, x2, y2) {
+        const distance = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+
+        return Number.isInteger(distance) ? 'valid' : 'invalid'
     }
 
 
-    console.log(`{${x1}, ${y1}} to {0, 0} is ${func(x1,y1,0,0)?'valid':'invalid'}`);
-    console.log(`{${x2}, ${y2}} to {0, 0} is ${func(x2,y2,0,0)?'valid':'invalid'}`);
-    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${func(x1,y1,x2,y2)?'valid':'invalid'}`);
+    return `{${x1}, ${y1}} to {0, 0} is ${getResult(x1, y1, 0, 0)}\n` +
+        `{${x2}, ${y2}} to {0, 0} is ${getResult(x2, y2, 0, 0)}\n` +
+        `{${x1}, ${y1}} to {${x2}, ${y2}} is ${getResult(x1, y1, x2, y2)}`;
 }
 
 console.log(validate([3, 0, 0, 4]));
