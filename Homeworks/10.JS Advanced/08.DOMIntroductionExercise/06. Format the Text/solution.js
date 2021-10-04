fe54f898b853927b5a5e1c8bc4b587553c067e89
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-10-04 13:23:08 +0300

    Add task 06

---
+++ Homeworks/10.JS Advanced/08.DOMIntroductionExercise/06. Format the Text/solution.js
@@ -0,0 +1,22 @@
+function solve() {
+  let input = document.getElementById('input').value.split('.');
+  let result = [];
+  let currString = '';
+  let counter = 0;
+  while (input.length != 1) {
+    counter++;
+    if (counter == 1) {
+      currString = '<p>'
+    }
+    currString += input.shift() + '. ';
+
+    if (counter === 3 || input.length === 1) {
+      currString += '</p>\n'
+      result.push(currString);
+      counter = 0;
+    }
+
+  }
+
+  document.getElementById('output').innerHTML = result.join('\n');
+}
