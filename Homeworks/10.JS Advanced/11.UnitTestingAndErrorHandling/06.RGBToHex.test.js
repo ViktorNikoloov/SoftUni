fe701bcf02d03dc1b8286f3d8f353222ddcb58b9
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-10-17 01:10:18 +0300

    Add task 06 - 11.UnitTestingAndErrorHandling

--- Homeworks/10.JS Advanced/11.UnitTestingAndErrorHandling/06.RGBToHex.test.js
+++ Homeworks/10.JS Advanced/11.UnitTestingAndErrorHandling/06.RGBToHex.test.js
@@ -1,46 +1,71 @@
-const {expect} = require('chai');
-const RGBtoHex = require('./06.RGBToHex');
+const { expect } = require('chai');
+const rgbToHexColor = require('./06.RGBToHex');
 
 
 describe('RGB number to Hex as string', () => {
-    it('return undefined when number is < 0', () => {
-        expect(RGBtoHex(-2, -3, -5)).to.be.undefined;
+
+    describe('Undefined path', () => {
+
+        it('return undefined when all number are < 0', () => {
+            expect(rgbToHexColor(-2, -3, -5)).to.be.undefined;
+        });
+
+        it('return undefined when first num is < 0', () => {
+            expect(rgbToHexColor(-2, 240, 230)).to.be.undefined;
+        });
+
+        it('return undefined when  third number is < 0', () => {
+            expect(rgbToHexColor(230, 240, -5)).to.be.undefined;
+        });
+
+        it('return undefined when second number is < 0', () => {
+            expect(rgbToHexColor(240, -1, 250)).to.be.undefined;
+        });
+
+        it('return undefined when number is > 255', () => {
+            expect(rgbToHexColor(256, 270, 310)).to.be.undefined;
+        });
+
+        it('return undefined when input is char', () => {
+            expect(rgbToHexColor('a', 'b', 'c')).to.be.undefined;
+        });
+
+        it('return undefined when input is not a number', () => {
+            expect(rgbToHexColor('1', '2', '3')).to.be.undefined;
+        });
+
+        it('return undefined when number is not integer', () => {
+            expect(rgbToHexColor(2.2)).to.be.undefined;
+        });
+
+        it('return undefined when parameter count in not corrent', () => {
+            expect(rgbToHexColor(255)).to.be.undefined;
+        });
+
     });
 
-    it('return undefined when number is > 255', () => {
-        expect(RGBtoHex(256, 270, 310)).to.be.undefined;
-    });
+    describe('Right path', () => {
 
-    it('return undefined when input is char', () => {
-        expect(RGBtoHex('a', 'b', 'c')).to.be.undefined;
-    });
+        it('cover black to hex', () => {
+            expect(rgbToHexColor(0, 0, 0)).to.equal('#000000');
+        });
 
-    it('return undefined when input is not a number', () => {
-        expect(RGBtoHex('1', '2', '3')).to.be.undefined;
-    });
+        it('cover white to hex', () => {
+            expect(rgbToHexColor(255, 255, 255)).to.equal('#FFFFFF');
+        });
 
-    it('return undefined when number is not integer', () => {
-        expect(RGBtoHex(2.2)).to.be.undefined;
-    });
+        it('cover red to hex', () => {
+            expect(rgbToHexColor(255, 0, 0)).to.equal('#FF0000');
+        });
 
-    it('cover black to hex', () => {
-        expect(RGBtoHex(0, 0, 0)).to.equal('#000000');
-    });
+        it('cover green to hex', () => {
+            expect(rgbToHexColor(0, 255, 0)).to.equal('#00FF00');
+        });
 
-    it('cover white to hex', () => {
-        expect(RGBtoHex(255, 255, 255)).to.equal('#FFFFFF');
-    });
+        it('cover blue to hex', () => {
+            expect(rgbToHexColor(0, 0, 255)).to.equal('#0000FF');
+        });
 
-    it('cover red to hex', () => {
-        expect(RGBtoHex(255, 0, 0)).to.equal('#FF0000');
-    });
-
-    it('cover green to hex', () => {
-        expect(RGBtoHex(0, 255, 0)).to.equal('#00FF00');
-    });
-
-    it('cover blue to hex', () => {
-        expect(RGBtoHex(0, 0, 255)).to.equal('#0000FF');
     });
 
 });
