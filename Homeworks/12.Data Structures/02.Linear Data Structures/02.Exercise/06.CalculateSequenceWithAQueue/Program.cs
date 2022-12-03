e25d26e568b8e09d9fe593804015cc907788e27c
Viktor Nikolov <viktor.g.nikolov@gmail.com>
2022-12-03 21:44:37 +0200

    Complete task 6

---
+++ Homeworks/12.Data Structures/02.Linear Data Structures/02.Exercise/06.CalculateSequenceWithAQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿var input = int.Parse(Console.ReadLine());
+
+Queue<int> queue = new();
+Queue<int> result = new();
+
+for (int i = 0; i < 50; i++)
+{
+    if (queue.Count == 0)
+    {
+        queue.Enqueue(input);
+        result.Enqueue(input);
+    }
+
+    var velue = queue.Dequeue();
+
+    int valueOne = velue + 1;
+    int valueTwo = 2 * velue + 1;
+    int valueThree = velue + 2;
+
+    queue.Enqueue(valueOne);
+    queue.Enqueue(valueTwo);
+    queue.Enqueue(valueThree);
+
+    result.Enqueue(valueOne);
+    result.Enqueue(valueTwo);
+    result.Enqueue(valueThree);
+}
+
+Console.WriteLine(String.Join(", ", result));
