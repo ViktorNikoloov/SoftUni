a1d1737b0e0d19fe46005a854ad8bc1f9660ff3f
Viktor Nikolov <viktor.g.nikolov@gmail.com>
2022-12-03 21:44:24 +0200

    Complete task 5

---
+++ Homeworks/12.Data Structures/02.Linear Data Structures/02.Exercise/05.ReverseNumbersWithAStack/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(x=>int.Parse(x))
+    .ToList();
+
+if(input is null || !input.Any())
+{
+    Console.WriteLine("empty");
+}
+
+Stack<int> stack= new();
+
+input.ForEach(x => stack.Push(x));
+
+Console.WriteLine(String.Join(' ', stack));
+
+//   Input	        Output
+// 1 2 3 4 5	   5 4 3 2 1
+//     1	           1
+//  (empty)	     (empty)
+//   1 -2	      -2 1
