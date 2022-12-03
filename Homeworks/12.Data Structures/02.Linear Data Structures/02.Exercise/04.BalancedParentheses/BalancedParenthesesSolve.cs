61066af0cf49a1d852a0cbe09001dac2d57c974e
Viktor Nikolov <viktor.g.nikolov@gmail.com>
2022-12-03 20:51:15 +0200

    Task 4 completed

---
+++ Homeworks/12.Data Structures/02.Linear Data Structures/02.Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,50 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            if (string.IsNullOrWhiteSpace(parentheses) || parentheses.Length % 2 == 1)
+            {
+                return false;
+            }
+
+            var stack = new Stack<char>(parentheses.Length / 2);
+
+            foreach (var currentCharacter in parentheses)
+            {
+                char expectedCharacter = default;
+
+                switch (currentCharacter)
+                {
+                    case ')':
+                        expectedCharacter = '(';
+                        break;
+                    case ']':
+                        expectedCharacter = '[';
+                        break;
+                    case '}':
+                        expectedCharacter = '{';
+                        break;
+                    default:
+                        stack.Push(currentCharacter);
+                        break;
+                }
+
+                if (expectedCharacter == default)
+                {
+                    continue;
+                }
+
+                if (stack.Pop() != expectedCharacter)
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
